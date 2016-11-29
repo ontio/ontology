b2866de10b6d9aa282ceed8cc020ab8dcd367537
Yanbo Li <dreamfly281@gmail.com>
2016-11-29 11:46:19 +0800

    Read the protocol config file
    The are kinds of configuration file used to define the
    user inputting parameters, add the config.go which used
    to access these configuration files
    
    Signed-off-by: Yanbo Li <dreamfly281@gmail.com>

---
+++ config/config.go
@@ -0,0 +1,52 @@
+package config
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+const (
+	defaultConfigFilename = "./config.json"
+)
+
+type ProtocolConfiguration struct {
+	Magic         int64    `json:"Magic"`
+	CoinVersion   int      `json:"CoinVersion"`
+	StandbyMiners []string `json:"StandbyMiners"`
+	SeedList      []string `json:"SeedList"`
+}
+
+type ProtocolFile struct {
+	ProtocolConfig ProtocolConfiguration `json:"ProtocolConfiguration"`
+}
+
+func SeedNodes() []string {
+	file, e := ioutil.ReadFile("./config/protocol.json")
+	if e != nil {
+		log.Fatalf("File error: %v\n", e)
+		os.Exit(1)
+	}
+	// Remove the UTF-8 Byte Order Mark
+	file = bytes.TrimPrefix(file, []byte("\xef\xbb\xbf"))
+
+	config := ProtocolFile{}
+	e = json.Unmarshal(file, &config)
+	if e != nil {
+		log.Fatalf("Unmarshal json file erro %v", e)
+	}
+	log.Printf("Protocol configuration: %v\n", config)
+	return config.ProtocolConfig.SeedList
+}
+
+// filesExists reports whether the named file or directory exists.
+func fileExists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
