b813034291a501c40e7ef7272a7530f9e74d62a6
Yanbo Li <dreamfly281@gmail.com>
2017-01-19 11:47:50 +0800

    Wrap the random number generate function
    The crypto algorithm need the "real" random number to
    make the implementation safe. The random generate function
    implemented here should be used in any place which need
    such kind of guarantee
    
    Signed-off-by: Yanbo Li <dreamfly281@gmail.com>

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,5 +1,9 @@
 package crypto
 
+import (
+	"crypto/rand"
+)
+
 func Sha256(value []byte) []byte{
 	//TODO: implement Sha256
 
@@ -10,4 +14,10 @@ func RIPEMD160(value []byte) []byte{
 	//TODO: implement RIPEMD160
 
 	return nil
+}
+
+// Generate the "real" random number which can be used for crypto algorithm
+func RandomNum(n int) ([]byte, error) {
+	// Get the random number from System urandom
+	return rand.GenerateRandomBytes(n)
 }
