c138ca7fc2e146e812b80a3b43872767404a7c1c
luodan.wg <luodan.wg@gmail.com>
2017-01-20 15:26:40 +0800

    upd

--- core/contract/program/Program.go
+++ core/contract/program/Program.go
@@ -1,10 +1,12 @@
 package program
 
 import (
-	"io"
-	"GoOnchain/common/serialization"
 	"GoOnchain/common"
+	"GoOnchain/common/serialization"
+	. "GoOnchain/errors"
+	"io"
 )
+
 type Program struct {
 
 	//the contract program code,which will be run on VM or specific envrionment
@@ -15,13 +17,33 @@ type Program struct {
 }
 
 //Serialize the Program
-func (p *Program) Serialize(w io.Writer)  {
-	serialization.WriteVarBytes(w,p.Parameter);
-	serialization.WriteVarBytes(w,p.Code);
+func (p *Program) Serialize(w io.Writer) error {
+	err := serialization.WriteVarBytes(w, p.Parameter)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "Execute Program Serialize Code failed.")
+	}
+	err = serialization.WriteVarBytes(w, p.Code)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "Execute Program Serialize Parameter failed.")
+	}
+	return nil
 }
 
+//Deserialize the Program
+func (p *Program) Deserialize(w io.Reader) error {
+	val, err := serialization.ReadVarBytes(w)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "Execute Program Deserialize Code failed.")
+	}
+	p.Code = val
+	p.Parameter, err = serialization.ReadVarBytes(w)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "Execute Program Deserialize Parameter failed.")
+	}
+	return nil
+}
 
-func (p *Program) CodeHash() common.Uint160{
+func (p *Program) CodeHash() common.Uint160 {
 	//TODO: implement to code hash
 	//new UInt160(script.Sha256().RIPEMD160());
 
