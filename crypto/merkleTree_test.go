fb86d7fb47abb5994da1098c2abf8cd3b5faedba
luodan.wg <luodan.wg@gmail.com>
2017-01-23 14:59:42 +0800

    implement of merkleTree's ComputeRoot func

---
+++ crypto/merkleTree_test.go
@@ -0,0 +1,26 @@
+package crypto
+
+import (
+	. "GoOnchain/common"
+	"crypto/sha256"
+	"fmt"
+	"testing"
+)
+
+func TestHash(t *testing.T) {
+
+	var data []Uint256
+	a1 := Uint256(sha256.Sum256([]byte("a")))
+	a2 := Uint256(sha256.Sum256([]byte("b")))
+	a3 := Uint256(sha256.Sum256([]byte("c")))
+	a4 := Uint256(sha256.Sum256([]byte("d")))
+	a5 := Uint256(sha256.Sum256([]byte("e")))
+	data = append(data, a1)
+	data = append(data, a2)
+	data = append(data, a3)
+	data = append(data, a4)
+	data = append(data, a5)
+	x, _ := ComputeRoot(data)
+	fmt.Printf("[Root Hash]:%x\n", *x)
+
+}
