20f2b65cc2f83aee1f9e5c91b9a7f7540639885a
zhengq1 <ljqlyy@gmail.com>
2017-04-05 18:00:59 +0800

    modify batch put && leveldbquery tool

--- core/store/LevelDBStore/LevelDBStore.go
+++ core/store/LevelDBStore/LevelDBStore.go
@@ -1,15 +1,16 @@
 package LevelDBStore
 
 import (
-	. "github.com/DNAProject/DNA/common"
-	"github.com/DNAProject/DNA/common/log"
-	"github.com/DNAProject/DNA/common/serialization"
-	. "github.com/DNAProject/DNA/core/asset"
-	"github.com/DNAProject/DNA/core/contract/program"
-	. "github.com/DNAProject/DNA/core/ledger"
-	tx "github.com/DNAProject/DNA/core/transaction"
-	"github.com/DNAProject/DNA/core/validation"
-	. "github.com/DNAProject/DNA/errors"
+	. "DNA/common"
+	"DNA/common/log"
+	"DNA/common/serialization"
+	. "DNA/core/asset"
+	"DNA/core/contract/program"
+	. "DNA/core/ledger"
+	tx "DNA/core/transaction"
+	"DNA/core/transaction/payload"
+	"DNA/core/validation"
+	. "DNA/errors"
 	"bytes"
 	"fmt"
 	"github.com/syndtr/goleveldb/leveldb"
@@ -122,24 +123,6 @@ func (self *LevelDBStore) NewIterator(options *opt.ReadOptions) *Iterator {
 	}
 }
 
-func (self *LevelDBStore) BatchPut(key []byte, value []byte) error {
-	self.b.Put(key, value)
-
-	return nil
-}
-
-func (self *LevelDBStore) BatchDelete(key []byte) error {
-
-	self.b.Delete(key)
-
-	return nil
-}
-
-func (self *LevelDBStore) BatchWrite() error {
-
-	return self.db.Write(self.b, nil)
-}
-
 func (bd *LevelDBStore) InitLedgerStore(l *Ledger) error {
 	// TODO: InitLedgerStore
 	return nil
@@ -181,6 +164,8 @@ func (bd *LevelDBStore) GetBlockHash(height uint32) (Uint256, error) {
 }
 
 func (bd *LevelDBStore) GetCurrentBlockHash() Uint256 {
+	bd.mutex.Lock()
+	defer bd.mutex.Unlock()
 
 	return bd.header_index[bd.current_block_height]
 }
@@ -305,7 +290,7 @@ func (bd *LevelDBStore) GetHeader(hash Uint256) (*Header, error) {
 	return h, err
 }
 
-func (bd *LevelDBStore) SaveAsset(asset *Asset) error {
+func (bd *LevelDBStore) SaveAsset(assetid Uint256, asset *Asset) error {
 	w := bytes.NewBuffer(nil)
 
 	asset.Serialize(w)
@@ -315,7 +300,7 @@ func (bd *LevelDBStore) SaveAsset(asset *Asset) error {
 	// add asset prefix.
 	assetkey.WriteByte(byte(ST_QuantityIssued))
 	// contact asset id
-	asset.ID.Serialize(assetkey)
+	assetid.Serialize(assetkey)
 
 	log.Debug(fmt.Sprintf("asset key: %x\n", assetkey))
 
@@ -365,8 +350,8 @@ func (bd *LevelDBStore) GetNextBlockHash(hash []byte) common.Uint256 {
 */
 
 func (bd *LevelDBStore) GetTransaction(hash Uint256) (*tx.Transaction, error) {
-	Trace()
-	log.Debug(fmt.Sprintf("GetTransaction Hash: %x\n", hash))
+	//Trace()
+	//log.Debug(fmt.Sprintf("GetTransaction Hash: %x\n", hash))
 	t := new(tx.Transaction)
 	err := bd.getTx(t, hash)
 
@@ -390,12 +375,12 @@ func (bd *LevelDBStore) getTx(tx *tx.Transaction, hash Uint256) error {
 	r := bytes.NewReader(tHash)
 
 	// get height
-	//_, err := serialization.ReadUint32(r)
-	height, err := serialization.ReadUint32(r)
+	_, err := serialization.ReadUint32(r)
+	//height, err := serialization.ReadUint32(r)
 	if err != nil {
 		return err
 	}
-	log.Debug(fmt.Sprintf("tx height: %d\n", height))
+	//log.Debug(fmt.Sprintf("tx height: %d\n", height))
 
 	// Deserialize Transaction
 	err = tx.Deserialize(r)
@@ -442,17 +427,17 @@ func (bd *LevelDBStore) GetBlock(hash Uint256) (*Block, error) {
 		//TODO: implement error process
 		return nil, err_get
 	}
-	log.Debug(fmt.Sprintf("GetBlock Data: %x\n", bHash))
+	//log.Debug(fmt.Sprintf("GetBlock Data: %x\n", bHash))
 
 	r := bytes.NewReader(bHash)
 
 	// first 8 bytes is sys_fee
-	//_,err := serialization.ReadUint64(r)
-	sysfee, err := serialization.ReadUint64(r)
+	_,err := serialization.ReadUint64(r)
+	//sysfee, err := serialization.ReadUint64(r)
 	if err != nil {
 		return nil, err
 	}
-	log.Debug(fmt.Sprintf("sysfee: %d\n", sysfee))
+	//log.Debug(fmt.Sprintf("sysfee: %d\n", sysfee))
 
 	// Deserialize block data
 	err = b.Deserialize(r)
@@ -473,6 +458,8 @@ func (bd *LevelDBStore) GetBlock(hash Uint256) (*Block, error) {
 
 func (bd *LevelDBStore) persist(b *Block) error {
 
+	batch := new(leveldb.Batch)
+
 	//////////////////////////////////////////////////////////////
 	// generate key with DATA_Header prefix
 	bhhash := bytes.NewBuffer(nil)
@@ -489,17 +476,19 @@ func (bd *LevelDBStore) persist(b *Block) error {
 	serialization.WriteUint64(w, sysfee)
 	b.Serialize(w)
 
-	// PUT VALUE
+	// BATCH PUT VALUE
+	batch.Put(bhhash.Bytes(), w.Bytes())
+	/*
 	err := bd.Put(bhhash.Bytes(), w.Bytes())
 	if err != nil {
 		return err
-	}
+	}*/
 
 	//////////////////////////////////////////////////////////////
 	// generate key with DATA_BlockHash prefix
 	bhash := bytes.NewBuffer(nil)
 	bhash.WriteByte(byte(DATA_BlockHash))
-	err = serialization.WriteUint32(bhash, b.Blockdata.Height)
+	err := serialization.WriteUint32(bhash, b.Blockdata.Height)
 	if err != nil {
 		return err
 	}
@@ -511,11 +500,13 @@ func (bd *LevelDBStore) persist(b *Block) error {
 	hashvalue.Serialize(hashwriter)
 	log.Debug(fmt.Sprintf("DATA_BlockHash table value: %x\n", hashvalue))
 
-	// PUT VALUE
+	// BATCH PUT VALUE
+	batch.Put(bhash.Bytes(), hashwriter.Bytes())
+	/*
 	err = bd.Put(bhash.Bytes(), hashwriter.Bytes())
 	if err != nil {
 		return err
-	}
+	}*/
 
 	//////////////////////////////////////////////////////////////
 	// save transcations to leveldb
@@ -530,9 +521,19 @@ func (bd *LevelDBStore) persist(b *Block) error {
 			}
 		*/
 
-		// now support RegisterAsset and Miner tx ONLY.
-		if b.Transcations[i].TxType == 0x40 || b.Transcations[i].TxType == 0x00 {
-			bd.SaveTransaction(b.Transcations[i], b.Blockdata.Height)
+		// now support RegisterAsset / IssueAsset / TransferAsset and Miner TX ONLY.
+		if b.Transcations[i].TxType == tx.RegisterAsset || b.Transcations[i].TxType == tx.IssueAsset || b.Transcations[i].TxType == tx.TransferAsset || b.Transcations[i].TxType == tx.BookKeeping {
+			err = bd.SaveTransaction(b.Transcations[i], b.Blockdata.Height)
+			if err != nil {
+				return err
+			}
+		}
+		if b.Transcations[i].TxType == tx.RegisterAsset {
+			ar := b.Transcations[i].Payload.(*payload.AssetRegistration)
+			err = bd.SaveAsset(b.Transcations[i].Hash(),ar.Asset)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
@@ -548,14 +549,21 @@ func (bd *LevelDBStore) persist(b *Block) error {
 	blockhash.Serialize(currentblock)
 	serialization.WriteUint32(currentblock, b.Blockdata.Height)
 
-	// PUT VALUE
+	// BATCH PUT VALUE
+	batch.Put(currentblockkey.Bytes(), currentblock.Bytes())
+	/*
 	err = bd.Put(currentblockkey.Bytes(), currentblock.Bytes())
 	if err != nil {
 		return err
-	}
+	}*/
 
 	//bh := b.Blockdata.Hash()
 	//bd.header_index[bd.current_block_height] = &bh
+
+	err = bd.db.Write(batch, nil)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
