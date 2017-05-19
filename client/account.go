e0361e4629952fcda01fa40aacaf68018255c5d1
zhengq1 <ljqlyy@gmail.com>
2017-05-19 17:09:02 +0800

    Update account.go

--- client/account.go
+++ client/account.go
@@ -4,10 +4,7 @@ import (
 	. "DNA/common"
 	"DNA/crypto"
 	. "DNA/errors"
-	"crypto/sha256"
 	"errors"
-	"github.com/tv42/base58"
-	"math/big"
 )
 
 type Account struct {
@@ -66,35 +63,4 @@ func (ac *Account) PrivKey() []byte {
 //get signer's public key
 func (ac *Account) PubKey() *crypto.PubKey {
 	return ac.PublicKey
-}
-
-func ToAddress(scriptHash []byte) string {
-	//fmt.Printf( "%x\n", scriptHash )
-	data := append([]byte{23}, scriptHash...)
-	temp := sha256.Sum256(data)
-	temps := sha256.Sum256(temp[:])
-	data = append(data, temps[0:4]...)
-
-	//data = append( []byte{0x00}, data... )
-	//fmt.Printf( "%x\n", data )
-
-	// Reverse
-	// base58.EncodeBig alread Reverse
-
-	//var dataint = make([]int,len(data))
-	//for i:=0; i<len(data); i++ {
-	//	dataint[i] = int(data[i])
-	//}
-	//for i:=0; i<len(dataint); i++ {
-	//	data[i] = byte(dataint[len(dataint)-1-i])
-	//}
-	//fmt.Printf( "%x\n", data )
-
-	bi := new(big.Int)
-	bi.SetBytes(data)
-	var dst []byte
-	dst = base58.EncodeBig(dst, bi)
-	//fmt.Printf( "%x\n", dst )
-
-	return string(dst[:])
 }
