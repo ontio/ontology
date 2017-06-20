a792536a21d83b9c05cd2a24326e057120ec1f53
Ziyan Zhou <zhouziyan@hotmail.com>
2017-06-20 12:52:16 +0800

    Bug fix for missing nonce attribute serialization
    After the transaction nonce moved to attribute, the
    serialization/deserialization of Nonce type are missing.
    
    Signed-off-by: Ziyan Zhou <zhouziyan@hotmail.com>

--- core/transaction/txAttribute.go
+++ core/transaction/txAttribute.go
@@ -16,6 +16,11 @@ const (
 	Description    TransactionAttributeUsage = 0x90
 )
 
+func IsValidAttributeType(usage TransactionAttributeUsage) bool {
+	return usage == Nonce || usage == Script ||
+		usage == DescriptionUrl || usage == Description
+}
+
 type TxAttribute struct {
 	Usage TransactionAttributeUsage
 	Data  []byte
@@ -36,18 +41,14 @@ func (u *TxAttribute) GetSize() uint32 {
 }
 
 func (tx *TxAttribute) Serialize(w io.Writer) error {
-	err := serialization.WriteUint8(w, byte(tx.Usage))
-	if err != nil {
+	if err := serialization.WriteUint8(w, byte(tx.Usage)); err != nil {
 		return NewDetailErr(err, ErrNoCode, "Transaction attribute Usage serialization error.")
 	}
-	// TODO other transaction attribute type
-	if tx.Usage == Description || tx.Usage == DescriptionUrl {
-		err := serialization.WriteVarBytes(w, tx.Data)
-		if err != nil {
-			return NewDetailErr(err, ErrNoCode, "Transaction attribute Data serialization error.")
-		}
-	} else {
+	if !IsValidAttributeType(tx.Usage) {
 		return NewDetailErr(errors.New("[TxAttribute] error"), ErrNoCode, "Unsupported attribute Description.")
+	}
+	if err := serialization.WriteVarBytes(w, tx.Data); err != nil {
+		return NewDetailErr(err, ErrNoCode, "Transaction attribute Data serialization error.")
 	}
 	return nil
 }
@@ -58,13 +59,13 @@ func (tx *TxAttribute) Deserialize(r io.Reader) error {
 		return NewDetailErr(err, ErrNoCode, "Transaction attribute Usage deserialization error.")
 	}
 	tx.Usage = TransactionAttributeUsage(val[0])
-	if tx.Usage == Description || tx.Usage == DescriptionUrl {
-		tx.Data, err = serialization.ReadVarBytes(r)
-		if err != nil {
-			return NewDetailErr(err, ErrNoCode, "Transaction attribute Data deserialization error.")
-		}
-	} else {
-		return NewDetailErr(errors.New("[TxAttribute] error"), ErrNoCode, "Unsupported attribute description.")
+	if !IsValidAttributeType(tx.Usage) {
+		return NewDetailErr(errors.New("[TxAttribute] error"), ErrNoCode, "Unsupported attribute Description.")
+	}
+	tx.Data, err = serialization.ReadVarBytes(r)
+	if err != nil {
+		return NewDetailErr(err, ErrNoCode, "Transaction attribute Data deserialization error.")
 	}
 	return nil
+
 }
