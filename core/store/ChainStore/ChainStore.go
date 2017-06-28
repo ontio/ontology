294c983621a98c67e58c5ed6cfd22d610b824ebc
bing <wangbing@onchain.com>
2017-06-28 14:48:39 +0800

    Fix utxo double spend check failure bug Signed-off-by: wangbing <wangbing@onchain.com>

--- core/store/ChainStore/ChainStore.go
+++ core/store/ChainStore/ChainStore.go
@@ -827,10 +827,11 @@ func (bd *ChainStore) persist(b *Block) error {
 			}
 
 			// find Transactions[i].UTXOInputs[index].ReferTxOutputIndex and delete it
-			for k := 0; k < len(unspents[txhash]); k++ {
-				if unspents[txhash][k] == uint16(b.Transactions[i].UTXOInputs[index].ReferTxOutputIndex) {
-					unspents[txhash] = append(unspents[txhash], unspents[txhash][:k]...)
-					unspents[txhash] = append(unspents[txhash], unspents[txhash][k+1:]...)
+			unspentLen := len(unspents[txhash])
+			for k, outputIndex := range unspents[txhash]{
+				if outputIndex == uint16(b.Transactions[i].UTXOInputs[index].ReferTxOutputIndex){
+					unspents[txhash][k] =  unspents[txhash][unspentLen - 1]
+					unspents[txhash] = unspents[txhash][:unspentLen-1]
 					break
 				}
 			}
