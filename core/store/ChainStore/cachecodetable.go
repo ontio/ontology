c62675f5099a0f2042246d51d80b7c515d4dd5e4
laizy <laizhichao@onchain.com>
2017-12-27 11:46:56 +0800

    fix nil pointer dereference
    Signed-off-by: laizy <laizhichao@onchain.com>

--- core/store/ChainStore/cachecodetable.go
+++ core/store/ChainStore/cachecodetable.go
@@ -1,9 +1,11 @@
 package ChainStore
 
 import (
+	"github.com/Ontology/core/states"
 	"github.com/Ontology/core/store"
 	"github.com/Ontology/errors"
-	"github.com/Ontology/core/states"
+
+	"fmt"
 )
 
 type CacheCodeTable struct {
@@ -11,9 +13,10 @@ type CacheCodeTable struct {
 }
 
 func (table *CacheCodeTable) GetCode(codeHash []byte) ([]byte, error) {
-	value, err := table.store.TryGet(store.ST_Contract, codeHash)
-	if err != nil {
-		return nil, errors.NewErr("[GetCode] TryGet contract error!")
+	value, _ := table.store.TryGet(store.ST_Contract, codeHash)
+	if value == nil {
+		return nil, errors.NewErr(fmt.Sprintf("[GetCode] TryGet contract error! codeHash:%x", codeHash))
 	}
+
 	return value.Value.(*states.ContractState).Code.Code, nil
 }
