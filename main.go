bba9ef8231935c619c953ec85d27ffff2e94596a
laizy <laizhichao@onchain.com>
2018-01-09 17:14:50 +0800

    fix defer statement
    Signed-off-by: laizy <laizhichao@onchain.com>
    
    (cherry picked from commit 6267669)

--- main.go
+++ main.go
@@ -11,15 +11,15 @@ import (
 	"github.com/Ontology/crypto"
 	"github.com/Ontology/net"
 	"github.com/Ontology/net/httpjsonrpc"
+	"github.com/Ontology/net/httpnodeinfo"
 	"github.com/Ontology/net/httprestful"
 	"github.com/Ontology/net/httpwebsocket"
-	"github.com/Ontology/net/httpnodeinfo"
 	"github.com/Ontology/net/protocol"
 	"os"
-	"runtime"
-	"time"
 	"os/signal"
+	"runtime"
 	"syscall"
+	"time"
 )
 
 const (
@@ -53,11 +53,12 @@ func main() {
 	log.Info("0. Loading the Ledger")
 	ledger.DefaultLedger = new(ledger.Ledger)
 	ledger.DefaultLedger.Store, err = ChainStore.NewLedgerStore()
-	defer ledger.DefaultLedger.Store.Close()
 	if err != nil {
 		log.Fatal("open LedgerStore err:", err)
 		os.Exit(1)
 	}
+	defer ledger.DefaultLedger.Store.Close()
+
 	ledger.DefaultLedger.Store.InitLedgerStore(ledger.DefaultLedger)
 	transaction.TxStore = ledger.DefaultLedger.Store
 	crypto.SetAlg(config.Parameters.EncryptAlg)
@@ -74,7 +75,7 @@ func main() {
 		goto ERROR
 	}
 	log.Debug("The Node's PublicKey ", acct.PublicKey)
-	ledger.StandbyBookKeepers, err= client.GetBookKeepers()
+	ledger.StandbyBookKeepers, err = client.GetBookKeepers()
 	if err != nil {
 		log.Fatalf("GetBookKeepers error:%s", err)
 		goto ERROR
@@ -143,6 +144,6 @@ func main() {
 		<-exit
 	}()
 
-	ERROR:
+ERROR:
 	os.Exit(1)
 }
