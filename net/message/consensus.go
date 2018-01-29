522b55a4670f44e36ee208de87b0b6066eab392a
laizy <laizhichao@onchain.com>
2018-01-29 16:32:44 +0800

    optimize ConsensusPayload's GetProgramHashes logic
    needless storage check.

--- net/message/consensus.go
+++ net/message/consensus.go
@@ -11,7 +11,6 @@ import (
 	"github.com/Ontology/common/serialization"
 	"github.com/Ontology/core/contract"
 	"github.com/Ontology/core/contract/program"
-	"github.com/Ontology/core/ledger"
 	sig "github.com/Ontology/core/signature"
 	"github.com/Ontology/core/validation"
 	"github.com/Ontology/crypto"
@@ -65,30 +64,13 @@ func (cp *ConsensusPayload) InvertoryType() common.InventoryType {
 }
 
 func (cp *ConsensusPayload) GetProgramHashes() ([]common.Uint160, error) {
-	log.Debug()
-
-	if ledger.DefaultLedger == nil {
-		return nil, errors.New("The Default ledger not exists.")
-	}
-	if cp.PrevHash != ledger.DefaultLedger.Store.GetCurrentBlockHash() {
-		return nil, errors.New("The PreHash Not matched.")
-	}
-
 	contract, err := contract.CreateSignatureContract(cp.Owner)
-	hash := contract.ProgramHash
-	log.Debug("program hash == ", hash)
-
-	//signatureRedeemScript, err := contract.CreateSignatureRedeemScript(bookKeepers[cp.BookKeeperIndex])
 	if err != nil {
 		return nil, NewDetailErr(err, ErrNoCode, "[Consensus], CreateSignatureContract failed.")
 	}
 
-	//hash, err:=common.ToCodeHash(signatureRedeemScript)
-	//if err != nil {
-	//	return  nil, NewDetailErr(err, ErrNoCode, "[Consensus], ToCodeHash failed.")
-	//}
-	programhashes := []common.Uint160{}
-	programhashes = append(programhashes, hash)
+	hash := contract.ProgramHash
+	programhashes := []common.Uint160{hash}
 	return programhashes, nil
 }
 
@@ -106,15 +88,13 @@ func (cp *ConsensusPayload) SetPrograms(programs []*program.Program) {
 }
 
 func (cp *ConsensusPayload) GetPrograms() []*program.Program {
-	cpg := []*program.Program{}
-	cpg = append(cpg, cp.Program)
+	cpg := []*program.Program{cp.Program}
 	return cpg
 }
 
 func (cp *ConsensusPayload) GetMessage() []byte {
 	//TODO: GetMessage
 	return sig.GetHashData(cp)
-	//return []byte{}
 }
 
 func (msg consensus) Handle(node Noder) error {
