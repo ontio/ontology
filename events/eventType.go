e324621096d9c9ecc990f6858b349d216762df2c
XiaoJie Guo <guoxiaojie@onchain.com>
2018-03-06 14:19:59 +0800

    Restore the files deleted when the branch merges
    Restore the files deleted when the branch merges
    
    Signed-off-by: XiaoJie Guo <guoxiaojie@onchain.com>
    
    # Conflicts:
    #	http/jsonrpc/RPCserver.go

--- events/eventType.go
+++ events/eventType.go
@@ -3,11 +3,10 @@ package events
 type EventType int16
 
 const (
-	EventSaveBlock               EventType = 0
-	EventReplyTx                 EventType = 1
-	EventBlockPersistCompleted   EventType = 2
-	EventNewInventory            EventType = 3
-	EventNodeDisconnect          EventType = 4
-	EventSmartCode               EventType = 5
-	EventNodeConsensusDisconnect EventType = 6
+	EventSaveBlock EventType = 0
+	EventReplyTx EventType = 1
+	EventBlockPersistCompleted EventType = 2
+	EventNewInventory EventType = 3
+	EventNodeDisconnect EventType = 4
+	EventSmartCode EventType = 5
 )
