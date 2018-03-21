0cb12abd5ec2f17ffe7aa1c95a54b5f00b579db1
xizho10 <josh_chow@163.com>
2018-03-21 17:18:32 +0800

    fix rpc request method null

--- http/base/rpc/rpc.go
+++ http/base/rpc/rpc.go
@@ -78,7 +78,10 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 		log.Error("HTTP JSON RPC Handle - json.Unmarshal: ", err)
 		return
 	}
-
+	if request["method"] == nil{
+		log.Error("HTTP JSON RPC Handle - method not found: ")
+		return
+	}
 	//get the corresponding function
 	function, ok := mainMux.m[request["method"].(string)]
 	if ok {
