ae16c37e676481fb9739942f67c2f3f45dfa720e
JunXi Xie <xiemylogos@gmail.com>
2018-04-04 15:02:03 +0800

    add unit test

---
+++ consensus/vbft/config/genesis_test.go
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package vconfig
+
+import (
+	"testing"
+
+	"github.com/Ontology/common/log"
+)
+
+const (
+	DefaultConfigFileName = "../../../consensus_config.json"
+)
+
+func TestGenConsensusPayload(t *testing.T) {
+	log.Init(log.PATH, log.Stdout)
+	res, err := genConsensusPayload(DefaultConfigFileName)
+	if err != nil {
+		t.Errorf("test failed: %v", err)
+	} else {
+		t.Logf("config content: %v\n", res)
+	}
+}
