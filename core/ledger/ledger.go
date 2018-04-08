laizy/ontology-old

expose merkle proof api

core/ledger/ledger.go
@@ -48,101 +48,101 @@ func NewLedger() (*Ledger, error) {
 	}, nil
 }
 
-func (this *Ledger) GetStore() store.LedgerStore {
-	return this.ldgStore
+func (self *Ledger) GetStore() store.LedgerStore {
+	return self.ldgStore
 }
 
-func (this *Ledger) Init(defaultBookkeeper []keypair.PublicKey) error {
+func (self *Ledger) Init(defaultBookkeeper []keypair.PublicKey) error {
 	genesisBlock, err := genesis.GenesisBlockInit(defaultBookkeeper)
 	if err != nil {
 		return fmt.Errorf("genesisBlock error %s", err)
 	}
-	err = this.ldgStore.InitLedgerStoreWithGenesisBlock(genesisBlock, defaultBookkeeper)
+	err = self.ldgStore.InitLedgerStoreWithGenesisBlock(genesisBlock, defaultBookkeeper)
 	if err != nil {
 		return fmt.Errorf("InitLedgerStoreWithGenesisBlock error %s", err)
 	}
 	return nil
 }
 
-func (this *Ledger) AddHeaders(headers []*types.Header) error {
-	return this.ldgStore.AddHeaders(headers)
+func (self *Ledger) AddHeaders(headers []*types.Header) error {
+	return self.ldgStore.AddHeaders(headers)
 }
 
-func (this *Ledger) AddBlock(block *types.Block) error {
-	return this.ldgStore.AddBlock(block)
+func (self *Ledger) AddBlock(block *types.Block) error {
+	return self.ldgStore.AddBlock(block)
 }
 
-func (this *Ledger) GetBlockRootWithNewTxRoot(txRoot common.Uint256) common.Uint256 {
-	return this.ldgStore.GetBlockRootWithNewTxRoot(txRoot)
+func (self *Ledger) GetBlockRootWithNewTxRoot(txRoot common.Uint256) common.Uint256 {
+	return self.ldgStore.GetBlockRootWithNewTxRoot(txRoot)
 }
 
-func (this *Ledger) GetBlockByHeight(height uint32) (*types.Block, error) {
-	return this.ldgStore.GetBlockByHeight(height)
+func (self *Ledger) GetBlockByHeight(height uint32) (*types.Block, error) {
+	return self.ldgStore.GetBlockByHeight(height)
 }
 
-func (this *Ledger) GetBlockByHash(blockHash common.Uint256) (*types.Block, error) {
-	return this.ldgStore.GetBlockByHash(blockHash)
+func (self *Ledger) GetBlockByHash(blockHash common.Uint256) (*types.Block, error) {
+	return self.ldgStore.GetBlockByHash(blockHash)
 }
 
-func (this *Ledger) GetHeaderByHeight(height uint32) (*types.Header, error) {
-	return this.ldgStore.GetHeaderByHeight(height)
+func (self *Ledger) GetHeaderByHeight(height uint32) (*types.Header, error) {
+	return self.ldgStore.GetHeaderByHeight(height)
 }
 
-func (this *Ledger) GetHeaderByHash(blockHash common.Uint256) (*types.Header, error) {
-	return this.ldgStore.GetHeaderByHash(blockHash)
+func (self *Ledger) GetHeaderByHash(blockHash common.Uint256) (*types.Header, error) {
+	return self.ldgStore.GetHeaderByHash(blockHash)
 }
 
-func (this *Ledger) GetBlockHash(height uint32) common.Uint256 {
-	return this.ldgStore.GetBlockHash(height)
+func (self *Ledger) GetBlockHash(height uint32) common.Uint256 {
+	return self.ldgStore.GetBlockHash(height)
 }
 
-func (this *Ledger) GetTransaction(txHash common.Uint256) (*types.Transaction, error) {
-	tx, _, err := this.ldgStore.GetTransaction(txHash)
+func (self *Ledger) GetTransaction(txHash common.Uint256) (*types.Transaction, error) {
+	tx, _, err := self.ldgStore.GetTransaction(txHash)
 	return tx, err
 }
 
-func (this *Ledger) GetTransactionWithHeight(txHash common.Uint256) (*types.Transaction, uint32, error) {
-	return this.ldgStore.GetTransaction(txHash)
+func (self *Ledger) GetTransactionWithHeight(txHash common.Uint256) (*types.Transaction, uint32, error) {
+	return self.ldgStore.GetTransaction(txHash)
 }
 
-func (this *Ledger) GetCurrentBlockHeight() uint32 {
-	return this.ldgStore.GetCurrentBlockHeight()
+func (self *Ledger) GetCurrentBlockHeight() uint32 {
+	return self.ldgStore.GetCurrentBlockHeight()
 }
 
-func (this *Ledger) GetCurrentBlockHash() common.Uint256 {
-	return this.ldgStore.GetCurrentBlockHash()
+func (self *Ledger) GetCurrentBlockHash() common.Uint256 {
+	return self.ldgStore.GetCurrentBlockHash()
 }
 
-func (this *Ledger) GetCurrentHeaderHeight() uint32 {
-	return this.ldgStore.GetCurrentHeaderHeight()
+func (self *Ledger) GetCurrentHeaderHeight() uint32 {
+	return self.ldgStore.GetCurrentHeaderHeight()
 }
 
-func (this *Ledger) GetCurrentHeaderHash() common.Uint256 {
-	return this.ldgStore.GetCurrentHeaderHash()
+func (self *Ledger) GetCurrentHeaderHash() common.Uint256 {
+	return self.ldgStore.GetCurrentHeaderHash()
 }
 
-func (this *Ledger) IsContainTransaction(txHash common.Uint256) (bool, error) {
-	return this.ldgStore.IsContainTransaction(txHash)
+func (self *Ledger) IsContainTransaction(txHash common.Uint256) (bool, error) {
+	return self.ldgStore.IsContainTransaction(txHash)
 }
 
-func (this *Ledger) IsContainBlock(blockHash common.Uint256) (bool, error) {
-	return this.ldgStore.IsContainBlock(blockHash)
+func (self *Ledger) IsContainBlock(blockHash common.Uint256) (bool, error) {
+	return self.ldgStore.IsContainBlock(blockHash)
 }
 
-func (this *Ledger) GetCurrentStateRoot() (common.Uint256, error) {
+func (self *Ledger) GetCurrentStateRoot() (common.Uint256, error) {
 	return common.Uint256{}, nil
 }
 
-func (this *Ledger) GetBookkeeperState() (*states.BookkeeperState, error) {
-	return this.ldgStore.GetBookkeeperState()
+func (self *Ledger) GetBookkeeperState() (*states.BookkeeperState, error) {
+	return self.ldgStore.GetBookkeeperState()
 }
 
-func (this *Ledger) GetStorageItem(codeHash common.Address, key []byte) ([]byte, error) {
+func (self *Ledger) GetStorageItem(codeHash common.Address, key []byte) ([]byte, error) {
 	storageKey := &states.StorageKey{
 		CodeHash: codeHash,
 		Key:      key,
 	}
-	storageItem, err := this.ldgStore.GetStorageItem(storageKey)
+	storageItem, err := self.ldgStore.GetStorageItem(storageKey)
 	if err != nil {
 		return nil, fmt.Errorf("GetStorageItem error %s", err)
 	}
@@ -152,18 +152,22 @@ func (this *Ledger) GetStorageItem(codeHash common.Address, key []byte) ([]byte,
 	return storageItem.Value, nil
 }
 
-func (this *Ledger) GetContractState(contractHash common.Address) (*payload.DeployCode, error) {
-	return this.ldgStore.GetContractState(contractHash)
+func (self *Ledger) GetContractState(contractHash common.Address) (*payload.DeployCode, error) {
+	return self.ldgStore.GetContractState(contractHash)
 }
 
-func (this *Ledger) PreExecuteContract(tx *types.Transaction) ([]interface{}, error) {
-	return this.ldgStore.PreExecuteContract(tx)
+func (self *Ledger) GetMerkleProof(m, n uint32) ([]common.Uint256, error) {
+	return self.ldgStore.GetMerkleProof(m, n)
 }
 
-func (this *Ledger) GetEventNotifyByTx(tx common.Uint256) ([]*event.NotifyEventInfo, error) {
-	return this.ldgStore.GetEventNotifyByTx(tx)
+func (self *Ledger) PreExecuteContract(tx *types.Transaction) ([]interface{}, error) {
+	return self.ldgStore.PreExecuteContract(tx)
 }
 
-func (this *Ledger) GetEventNotifyByBlock(height uint32) ([]common.Uint256, error) {
-	return this.ldgStore.GetEventNotifyByBlock(height)
+func (self *Ledger) GetEventNotifyByTx(tx common.Uint256) ([]*event.NotifyEventInfo, error) {
+	return self.ldgStore.GetEventNotifyByTx(tx)
+}
+
+func (self *Ledger) GetEventNotifyByBlock(height uint32) ([]common.Uint256, error) {
+	return self.ldgStore.GetEventNotifyByBlock(height)
 }

core/store/ledgerstore/ledger_store.go
@@ -857,6 +857,10 @@ func (this *LedgerStoreImp) GetBookkeeperState() (*states.BookkeeperState, error
 	return this.stateStore.GetBookkeeperState()
 }
 
+func (this *LedgerStoreImp) GetMerkleProof(m, n uint32) ([]common.Uint256, error) {
+	return this.stateStore.GetMerkleProof(m, n)
+}
+
 func (this *LedgerStoreImp) GetContractState(contractHash common.Address) (*payload.DeployCode, error) {
 	return this.stateStore.GetContractState(contractHash)
 }

ontio/ontology

core/ledger/ledger.go
@@ -48,101 +48,101 @@ func NewLedger() (*Ledger, error) {
 	}, nil
 }
 
-func (this *Ledger) GetStore() store.LedgerStore {
-	return this.ldgStore
+func (self *Ledger) GetStore() store.LedgerStore {
+	return self.ldgStore
 }
 
-func (this *Ledger) Init(defaultBookkeeper []keypair.PublicKey) error {
+func (self *Ledger) Init(defaultBookkeeper []keypair.PublicKey) error {
 	genesisBlock, err := genesis.GenesisBlockInit(defaultBookkeeper)
 	if err != nil {
 		return fmt.Errorf("genesisBlock error %s", err)
 	}
-	err = this.ldgStore.InitLedgerStoreWithGenesisBlock(genesisBlock, defaultBookkeeper)
+	err = self.ldgStore.InitLedgerStoreWithGenesisBlock(genesisBlock, defaultBookkeeper)
 	if err != nil {
 		return fmt.Errorf("InitLedgerStoreWithGenesisBlock error %s", err)
 	}
 	return nil
 }
 
-func (this *Ledger) AddHeaders(headers []*types.Header) error {
-	return this.ldgStore.AddHeaders(headers)
+func (self *Ledger) AddHeaders(headers []*types.Header) error {
+	return self.ldgStore.AddHeaders(headers)
 }
 
-func (this *Ledger) AddBlock(block *types.Block) error {
-	return this.ldgStore.AddBlock(block)
+func (self *Ledger) AddBlock(block *types.Block) error {
+	return self.ldgStore.AddBlock(block)
 }
 
-func (this *Ledger) GetBlockRootWithNewTxRoot(txRoot common.Uint256) common.Uint256 {
-	return this.ldgStore.GetBlockRootWithNewTxRoot(txRoot)
+func (self *Ledger) GetBlockRootWithNewTxRoot(txRoot common.Uint256) common.Uint256 {
+	return self.ldgStore.GetBlockRootWithNewTxRoot(txRoot)
 }
 
-func (this *Ledger) GetBlockByHeight(height uint32) (*types.Block, error) {
-	return this.ldgStore.GetBlockByHeight(height)
+func (self *Ledger) GetBlockByHeight(height uint32) (*types.Block, error) {
+	return self.ldgStore.GetBlockByHeight(height)
 }
 
-func (this *Ledger) GetBlockByHash(blockHash common.Uint256) (*types.Block, error) {
-	return this.ldgStore.GetBlockByHash(blockHash)
+func (self *Ledger) GetBlockByHash(blockHash common.Uint256) (*types.Block, error) {
+	return self.ldgStore.GetBlockByHash(blockHash)
 }
 
-func (this *Ledger) GetHeaderByHeight(height uint32) (*types.Header, error) {
-	return this.ldgStore.GetHeaderByHeight(height)
+func (self *Ledger) GetHeaderByHeight(height uint32) (*types.Header, error) {
+	return self.ldgStore.GetHeaderByHeight(height)
 }
 
-func (this *Ledger) GetHeaderByHash(blockHash common.Uint256) (*types.Header, error) {
-	return this.ldgStore.GetHeaderByHash(blockHash)
+func (self *Ledger) GetHeaderByHash(blockHash common.Uint256) (*types.Header, error) {
+	return self.ldgStore.GetHeaderByHash(blockHash)
 }
 
-func (this *Ledger) GetBlockHash(height uint32) common.Uint256 {
-	return this.ldgStore.GetBlockHash(height)
+func (self *Ledger) GetBlockHash(height uint32) common.Uint256 {
+	return self.ldgStore.GetBlockHash(height)
 }
 
-func (this *Ledger) GetTransaction(txHash common.Uint256) (*types.Transaction, error) {
-	tx, _, err := this.ldgStore.GetTransaction(txHash)
+func (self *Ledger) GetTransaction(txHash common.Uint256) (*types.Transaction, error) {
+	tx, _, err := self.ldgStore.GetTransaction(txHash)
 	return tx, err
 }
 
-func (this *Ledger) GetTransactionWithHeight(txHash common.Uint256) (*types.Transaction, uint32, error) {
-	return this.ldgStore.GetTransaction(txHash)
+func (self *Ledger) GetTransactionWithHeight(txHash common.Uint256) (*types.Transaction, uint32, error) {
+	return self.ldgStore.GetTransaction(txHash)
 }
 
-func (this *Ledger) GetCurrentBlockHeight() uint32 {
-	return this.ldgStore.GetCurrentBlockHeight()
+func (self *Ledger) GetCurrentBlockHeight() uint32 {
+	return self.ldgStore.GetCurrentBlockHeight()
 }
 
-func (this *Ledger) GetCurrentBlockHash() common.Uint256 {
-	return this.ldgStore.GetCurrentBlockHash()
+func (self *Ledger) GetCurrentBlockHash() common.Uint256 {
+	return self.ldgStore.GetCurrentBlockHash()
 }
 
-func (this *Ledger) GetCurrentHeaderHeight() uint32 {
-	return this.ldgStore.GetCurrentHeaderHeight()
+func (self *Ledger) GetCurrentHeaderHeight() uint32 {
+	return self.ldgStore.GetCurrentHeaderHeight()
 }
 
-func (this *Ledger) GetCurrentHeaderHash() common.Uint256 {
-	return this.ldgStore.GetCurrentHeaderHash()
+func (self *Ledger) GetCurrentHeaderHash() common.Uint256 {
+	return self.ldgStore.GetCurrentHeaderHash()
 }
 
-func (this *Ledger) IsContainTransaction(txHash common.Uint256) (bool, error) {
-	return this.ldgStore.IsContainTransaction(txHash)
+func (self *Ledger) IsContainTransaction(txHash common.Uint256) (bool, error) {
+	return self.ldgStore.IsContainTransaction(txHash)
 }
 
-func (this *Ledger) IsContainBlock(blockHash common.Uint256) (bool, error) {
-	return this.ldgStore.IsContainBlock(blockHash)
+func (self *Ledger) IsContainBlock(blockHash common.Uint256) (bool, error) {
+	return self.ldgStore.IsContainBlock(blockHash)
 }
 
-func (this *Ledger) GetCurrentStateRoot() (common.Uint256, error) {
+func (self *Ledger) GetCurrentStateRoot() (common.Uint256, error) {
 	return common.Uint256{}, nil
 }
 
-func (this *Ledger) GetBookkeeperState() (*states.BookkeeperState, error) {
-	return this.ldgStore.GetBookkeeperState()
+func (self *Ledger) GetBookkeeperState() (*states.BookkeeperState, error) {
+	return self.ldgStore.GetBookkeeperState()
 }
 
-func (this *Ledger) GetStorageItem(codeHash common.Address, key []byte) ([]byte, error) {
+func (self *Ledger) GetStorageItem(codeHash common.Address, key []byte) ([]byte, error) {
 	storageKey := &states.StorageKey{
 		CodeHash: codeHash,
 		Key:      key,
 	}
-	storageItem, err := this.ldgStore.GetStorageItem(storageKey)
+	storageItem, err := self.ldgStore.GetStorageItem(storageKey)
 	if err != nil {
 		return nil, fmt.Errorf("GetStorageItem error %s", err)
 	}
@@ -152,18 +152,22 @@ func (this *Ledger) GetStorageItem(codeHash common.Address, key []byte) ([]byte,
 	return storageItem.Value, nil
 }
 
-func (this *Ledger) GetContractState(contractHash common.Address) (*payload.DeployCode, error) {
-	return this.ldgStore.GetContractState(contractHash)
+func (self *Ledger) GetContractState(contractHash common.Address) (*payload.DeployCode, error) {
+	return self.ldgStore.GetContractState(contractHash)
 }
 
-func (this *Ledger) PreExecuteContract(tx *types.Transaction) ([]interface{}, error) {
-	return this.ldgStore.PreExecuteContract(tx)
+func (self *Ledger) GetMerkleProof(proofHeight, rootHeight uint32) ([]common.Uint256, error) {
+	return self.ldgStore.GetMerkleProof(proofHeight, rootHeight)
 }
 
-func (this *Ledger) GetEventNotifyByTx(tx common.Uint256) ([]*event.NotifyEventInfo, error) {
-	return this.ldgStore.GetEventNotifyByTx(tx)
+func (self *Ledger) PreExecuteContract(tx *types.Transaction) ([]interface{}, error) {
+	return self.ldgStore.PreExecuteContract(tx)
 }
 
-func (this *Ledger) GetEventNotifyByBlock(height uint32) ([]common.Uint256, error) {
-	return this.ldgStore.GetEventNotifyByBlock(height)
+func (self *Ledger) GetEventNotifyByTx(tx common.Uint256) ([]*event.NotifyEventInfo, error) {
+	return self.ldgStore.GetEventNotifyByTx(tx)
+}
+
+func (self *Ledger) GetEventNotifyByBlock(height uint32) ([]common.Uint256, error) {
+	return self.ldgStore.GetEventNotifyByBlock(height)
 }

core/store/ledgerstore/ledger_store.go
@@ -639,6 +639,7 @@ func (this *LedgerStoreImp) saveBlockToStateStore(block *types.Block) error {
 			return fmt.Errorf("handleTransaction error %s", err)
 		}
 	}
+
 	err := this.stateStore.AddMerkleTreeRoot(block.Header.TransactionsRoot)
 	if err != nil {
 		return fmt.Errorf("AddMerkleTreeRoot error %s", err)
@@ -855,6 +856,10 @@ func (this *LedgerStoreImp) GetBlockByHeight(height uint32) (*types.Block, error
 
 func (this *LedgerStoreImp) GetBookkeeperState() (*states.BookkeeperState, error) {
 	return this.stateStore.GetBookkeeperState()
+}
+
+func (this *LedgerStoreImp) GetMerkleProof(proofHeight, rootHeight uint32) ([]common.Uint256, error) {
+	return this.stateStore.GetMerkleProof(proofHeight, rootHeight)
 }
 
 func (this *LedgerStoreImp) GetContractState(contractHash common.Address) (*payload.DeployCode, error) {