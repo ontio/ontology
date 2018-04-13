821c5a4fd6acf87a1887b763b8519f1f2e5119ec
laizy <laizhichao@onchain.com>
2018-04-13 16:13:53 +0800

    add validator test
    Signed-off-by: laizy <laizhichao@onchain.com>

---
+++ validator/stateless/stateless_validator_test.go
@@ -0,0 +1,60 @@
+package stateless
+
+import (
+	"testing"
+
+	"github.com/ontio/ontology-crypto/keypair"
+	"github.com/ontio/ontology-eventbus/actor"
+	"github.com/ontio/ontology/account"
+	"github.com/ontio/ontology/common/log"
+	"github.com/ontio/ontology/core/signature"
+	ctypes "github.com/ontio/ontology/core/types"
+	"github.com/ontio/ontology/core/utils"
+	"github.com/ontio/ontology/errors"
+	"github.com/ontio/ontology/smartcontract/types"
+	types2 "github.com/ontio/ontology/validator/types"
+	"github.com/stretchr/testify/assert"
+	"time"
+)
+
+func signTransaction(signer *account.Account, tx *ctypes.Transaction) error {
+	hash := tx.Hash()
+	sign, _ := signature.Sign(signer, hash[:])
+	tx.Sigs = append(tx.Sigs, &ctypes.Sig{
+		PubKeys: []keypair.PublicKey{signer.PublicKey},
+		M:       1,
+		SigData: [][]byte{sign},
+	})
+	return nil
+}
+
+func TestStatelessValidator(t *testing.T) {
+	log.Init(log.PATH, log.Stdout)
+	acc := account.NewAccount("")
+
+	code := types.VmCode{
+		VmType: types.NEOVM,
+		Code:   []byte{1, 2, 3},
+	}
+	tx := utils.NewDeployTransaction(code, "test", "1", "author", "author@123.com", "test desp", false)
+
+	signTransaction(acc, tx)
+
+	validator := &validator{id: "test"}
+	props := actor.FromProducer(func() actor.Actor {
+		return validator
+	})
+
+	pid, err := actor.SpawnNamed(props, validator.id)
+	assert.Nil(t, err)
+
+	msg := &types2.CheckTx{WorkerId: 1, Tx: *tx}
+	fut := pid.RequestFuture(msg, time.Second)
+
+	res, err := fut.Result()
+	assert.Nil(t, err)
+
+	result := res.(*types2.CheckResponse)
+	assert.Equal(t, result.ErrCode, errors.ErrNoError)
+	assert.Equal(t, tx.Hash(), result.Hash)
+}
