9bc80aed2ff3fb573cf224fe813808b40cfc4675
wangbing <wangbing@onchain.com>
2018-04-16 18:29:37 +0800

    add testcase
    signed-off-by:wangbing@onchain.com

--- core/store/statestore/state_batch_test.go
+++ core/store/statestore/state_batch_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/ontio/ontology/core/store/leveldbstore"
 	"os"
 	"testing"
-	//"github.com/syndtr/goleveldb/leveldb"
+	"github.com/syndtr/goleveldb/leveldb"
 )
 
 var (
@@ -50,6 +50,119 @@ func TestStateBatch_TryGetOrAdd(t *testing.T) {
 	storeItem := v.Value.(*states.StorageItem)
 	if string(storeItem.Value) != string(value.Value){
 		t.Errorf("TestStateBatch_TryGetOrAdd value:%s != %s", storeItem.Value, value.Value)
+		return
+	}
+}
+
+func TestStateBatch_TryAdd(t *testing.T) {
+	prefix := com.ST_STORAGE
+	key := []byte("foo1")
+	value := &states.StorageItem{Value: []byte("bar1")}
+
+	err := testBatch.TryGetOrAdd(prefix, key, value)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd TryGetOrAdd error:%s", err)
+		return
+	}
+
+	v , err := testBatch.TryGet(prefix, key)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd TryGet error:%s", err)
+		return
+	}
+
+	storeItem := v.Value.(*states.StorageItem)
+	if string(storeItem.Value) != string(value.Value){
+		t.Errorf("TestStateBatch_TryGetOrAdd value:%s != %s", storeItem.Value, value.Value)
+		return
+	}
+}
+
+func TestStateBatch_TryGetAndChange(t *testing.T) {
+	prefix := com.ST_STORAGE
+	key := []byte("foo")
+	value := &states.StorageItem{Value: []byte("bar")}
+
+	err := testBatch.TryGetOrAdd(prefix, key, value)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd TryGetOrAdd error:%s", err)
+		return
+	}
+
+	key1 := []byte("foo1")
+	value1 := &states.StorageItem{Value: []byte("bar1")}
+
+	err = testBatch.TryGetOrAdd(prefix, key1, value1)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd TryGetOrAdd error:%s", err)
+		return
+	}
+
+	testBatch.TryDelete(prefix, key)
+
+	v, err := testBatch.TryGetAndChange(prefix, key)
+	if err != nil {
+		t.Errorf("TryGetAndChange error:%s", err)
+		return
+	}
+
+	if v != nil {
+		t.Errorf("TryGetAndChange error key:%s should nil", key)
+		return
+	}
+
+	v1, err := testBatch.TryGetAndChange(prefix, key1)
+	if err != nil {
+		t.Errorf("TryGetAndChange error:%s", err)
+		return
+	}
+
+	storeItem := v1.(*states.StorageItem)
+	if string(storeItem.Value) != string(value1.Value){
+		t.Errorf("TestStateBatch_TryGetOrAdd value:%s != %s", storeItem.Value, value.Value)
+		return
+	}
+}
+
+func TestStateBatch_CommitTo(t *testing.T) {
+	prefix := com.ST_STORAGE
+	key := []byte("foo1")
+	value := &states.StorageItem{Value: []byte("bar1")}
+
+	err := testBatch.TryGetOrAdd(prefix, key, value)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd TryGetOrAdd error:%s", err)
+		return
+	}
+
+	testLevelDB.NewBatch()
+	err = testBatch.CommitTo()
+	if err != nil {
+		t.Errorf("CommitTo error:%s", err)
+		return
+	}
+
+	err = testLevelDB.BatchCommit()
+	if err != nil {
+		t.Errorf("BatchCommit error:%s", err)
+		return
+	}
+
+	data ,err := testLevelDB.Get(append([]byte{byte(prefix)}, key...))
+	if err != nil && err != leveldb.ErrNotFound {
+		t.Errorf("testLevelDB.Get error:%s", err)
+		return
+	}
+
+	item, err := getStateObject(prefix, data)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd getStateObject eror:%s", err)
+		return
+	}
+
+	v := item.(*states.StorageItem)
+	if string(v.Value) != string(value.Value) {
+		t.Errorf("TestStateBatch_TryGetOrAdd value:%s != %s", v.Value, value.Value)
 		return
 	}
 }
