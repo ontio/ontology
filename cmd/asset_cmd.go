19f5f1df11de9812f10f29eec13cd1951e7ffcf6
Yihen <liuyihen@gmail.com>
2018-04-16 11:44:49 +0800

    Head line: add cli implement first version.   add cmd package for cli module.   restruct main.go file.
      Signed-off-by:liuyihen@onchain.com

--- cmd/asset_cmd.go
+++ cmd/asset_cmd.go
@@ -16,7 +16,7 @@
  * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package transfer
+package cmd
 
 import (
 	"bytes"
@@ -27,49 +27,102 @@ import (
 	"os"
 	"time"
 
-	"github.com/urfave/cli"
-
 	"github.com/ontio/ontology-crypto/keypair"
 	"github.com/ontio/ontology/account"
-	clicommon "github.com/ontio/ontology/cli/common"
+	"github.com/ontio/ontology/cmd/utils"
 	"github.com/ontio/ontology/common"
+	"github.com/ontio/ontology/common/config"
 	"github.com/ontio/ontology/core/signature"
 	ctypes "github.com/ontio/ontology/core/types"
 	cutils "github.com/ontio/ontology/core/utils"
-	"github.com/ontio/ontology/http/base/rpc"
+	jrpc "github.com/ontio/ontology/http/base/rpc"
 	nstates "github.com/ontio/ontology/smartcontract/service/native/states"
 	"github.com/ontio/ontology/smartcontract/states"
 	vmtypes "github.com/ontio/ontology/smartcontract/types"
+	"github.com/urfave/cli"
 )
 
-func transferAction(c *cli.Context) error {
-	if c.NumFlags() == 0 {
-		cli.ShowSubcommandHelp(c)
-		return nil
+var (
+	AssetCommand = cli.Command{
+		Name:        "asset",
+		Usage:       "controll wallet,just as create delete action .etc",
+		ArgsUsage:   "",
+		Category:    "ASSET COMMANDS",
+		Description: `[reg/issue/transfer]`,
+		Subcommands: []cli.Command{
+			{
+				Action:      utils.MigrateFlags(regAsset),
+				Name:        "reg",
+				Usage:       "register asset",
+				Flags:       append(append(NodeFlags, RpcFlags...), WhisperFlags...),
+				Category:    "ASSET COMMANDS",
+				Description: ``,
+			},
+			{
+				Action:      utils.MigrateFlags(issueAsset),
+				Name:        "issue",
+				Usage:       "issue asset by command",
+				Flags:       append(append(NodeFlags, RpcFlags...), WhisperFlags...),
+				Category:    "ASSET COMMANDS",
+				Description: ``,
+			},
+			{
+				Action:      utils.MigrateFlags(transferAsset),
+				Name:        "transfer",
+				Usage:       "transfer asset",
+				Flags:       append(append(NodeFlags, RpcFlags...), WhisperFlags...),
+				Category:    "ASSET COMMANDS",
+				Description: ``,
+			},
+		},
 	}
-	// wallet name is wallet.dat by default
-	contract := c.String("contract")
+)
+
+func regAsset(ctx *cli.Context) error {
+	//TODO
+	return nil
+}
+
+func issueAsset(ctx *cli.Context) error {
+	//TODO
+	return nil
+}
+
+func signTransaction(signer *account.Account, tx *ctypes.Transaction) error {
+	hash := tx.Hash()
+	sign, _ := signature.Sign(signer, hash[:])
+	tx.Sigs = append(tx.Sigs, &ctypes.Sig{
+		PubKeys: []keypair.PublicKey{signer.PublicKey},
+		M:       1,
+		SigData: [][]byte{sign},
+	})
+	return nil
+}
+
+func transferAsset(ctx *cli.Context) error {
+	config.Init(ctx)
+	contract := ctx.GlobalString(utils.ContractAddrFlag.Name)
 	if contract == "" {
 		fmt.Println("Invalid contract address: ", contract)
 		os.Exit(1)
 	}
 	ct, _ := common.HexToBytes(contract)
 	ctu, _ := common.AddressParseFromBytes(ct)
-	from := c.String("from")
+	from := ctx.GlobalString(utils.TransactionFromFlag.Name)
 	if from == "" {
 		fmt.Println("Invalid sender address: ", from)
 		os.Exit(1)
 	}
 	f, _ := common.HexToBytes(from)
 	fu, _ := common.AddressParseFromBytes(f)
-	to := c.String("to")
+	to := ctx.GlobalString(utils.TransactionToFlag.Name)
 	if to == "" {
 		fmt.Println("Invalid revicer address: ", to)
 		os.Exit(1)
 	}
 	t, _ := common.HexToBytes(to)
 	tu, _ := common.AddressParseFromBytes(t)
-	value := c.Int64("value")
+	value := ctx.GlobalInt64(utils.TransactionValueFlag.Name)
 	if value <= 0 {
 		fmt.Println("Invalid ont amount: ", value)
 		os.Exit(1)
@@ -111,14 +164,10 @@ func transferAction(c *cli.Context) error {
 
 	tx.Nonce = uint32(time.Now().Unix())
 
-	passwd := c.String("password")
+	passwd := ctx.GlobalString(utils.UserPasswordFlag.Name)
 
 	acct := account.Open(account.WALLET_FILENAME, []byte(passwd))
 	acc := acct.GetDefaultAccount()
-	if acc == nil {
-		fmt.Println(" can not get default account")
-		os.Exit(1)
-	}
 
 	if err := signTransaction(acc, tx); err != nil {
 		fmt.Println("signTransaction error:", err)
@@ -130,8 +179,7 @@ func transferAction(c *cli.Context) error {
 		fmt.Println("Serialize transaction error.")
 		os.Exit(1)
 	}
-	resp, err := rpc.Call(clicommon.RpcAddress(), "sendrawtransaction", 0,
-		[]interface{}{hex.EncodeToString(txbf.Bytes())})
+	resp, err := jrpc.Call(localRpcAddress(), "sendrawtransaction", 0, []interface{}{hex.EncodeToString(txbf.Bytes())})
 
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -150,52 +198,6 @@ func transferAction(c *cli.Context) error {
 		fmt.Println(r["result"].(string))
 		os.Exit(1)
 	}
-	return nil
-}
 
-func signTransaction(signer *account.Account, tx *ctypes.Transaction) error {
-	hash := tx.Hash()
-	sign, _ := signature.Sign(signer, hash[:])
-	tx.Sigs = append(tx.Sigs, &ctypes.Sig{
-		PubKeys: []keypair.PublicKey{signer.PublicKey},
-		M:       1,
-		SigData: [][]byte{sign},
-	})
 	return nil
-}
-
-func NewCommand() *cli.Command {
-	return &cli.Command{
-		Name:        "transfer",
-		Usage:       "user ont transfer",
-		Description: "With nodectl transfer, you could transfer ont.",
-		ArgsUsage:   "[args]",
-		Flags: []cli.Flag{
-			cli.StringFlag{
-				Name:  "contract, c",
-				Usage: "contract address",
-			},
-			cli.StringFlag{
-				Name:  "from, f",
-				Usage: "sender address",
-			},
-			cli.StringFlag{
-				Name:  "to, t",
-				Usage: "revicer address",
-			},
-			cli.Int64Flag{
-				Name:  "value, v",
-				Usage: "ont amount",
-			},
-			cli.StringFlag{
-				Name:  "password, p",
-				Usage: "wallet password",
-				Value: "passwordtest",
-			},
-		},
-		Action: transferAction,
-		OnUsageError: func(c *cli.Context, err error, isSubcommand bool) error {
-			return cli.NewExitError("", 1)
-		},
-	}
 }
---
+++ cmd/utils/flags.go
@@ -0,0 +1,351 @@
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package utils
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/urfave/cli"
+)
+
+type DirectoryString struct {
+	Value string
+}
+
+type DirectoryFlag struct {
+	Name  string
+	Value DirectoryString
+	Usage string
+}
+
+var (
+	JSpathFlag = cli.StringFlag{
+		Name:  "jspath",
+		Usage: "JavaScript root path for `loadScript`",
+		Value: ".",
+	}
+
+	ExecFlag = cli.StringFlag{
+		Name:  "exec",
+		Usage: "Execute JavaScript statement",
+	}
+
+	PreloadJSFlag = cli.StringFlag{
+		Name:  "preload",
+		Usage: "Comma separated list of JavaScript files to preload into the console",
+	}
+
+	TestnetFlag = cli.BoolFlag{
+		Name:  "testnet",
+		Usage: "Ropsten network: pre-configured proof-of-work test network",
+	}
+
+	RinkebyFlag = cli.BoolFlag{
+		Name:  "rinkeby",
+		Usage: "Rinkeby network: pre-configured proof-of-authority test network",
+	}
+
+	DataDirFlag = DirectoryFlag{
+		Name:  "datadir",
+		Usage: "Data directory for the databases and keystore",
+		Value: DirectoryString{DefaultDataDir()},
+	}
+
+	EncryptTypeFlag = cli.StringFlag{
+		Name: "encrypt",
+		Usage: `assign encrypt type when use create wallet, just as:
+						SHA224withECDSA, SHA256withECDSA,
+						SHA384withECDSA, SHA512withECDSA,
+						SHA3-224withECDSA, SHA3-256withECDSA,
+						SHA3-384withECDSA, SHA3-512withECDSA,
+						RIPEMD160withECDSA, SM3withSM2, SHA512withEdDSA`,
+	}
+
+	WalletAddrFlag = cli.StringFlag{
+		Name: "addr",
+	}
+
+	WalletNameFlag = cli.StringFlag{
+		Name: "name",
+	}
+
+	WalletPwdFlag = cli.StringFlag{
+		Name: "pwd",
+	}
+
+	WalletUsedFlag = cli.StringFlag{
+		Name: "wallet",
+	}
+
+	ConfigUsedFlag = cli.StringFlag{
+		Name: "config",
+	}
+
+	// RPC settings
+	RPCEnabledFlag = cli.BoolFlag{
+		Name: "rpc",
+	}
+
+	WsEnabledFlag = cli.BoolFlag{
+		Name: "ws",
+	}
+
+	//information cmd settings
+	BHashInfoFlag = cli.StringFlag{
+		Name: "bhash",
+	}
+
+	BTrxInfoFlag = cli.StringFlag{
+		Name: "hash",
+	}
+
+	HeightInfoFlag = cli.StringFlag{
+		Name: "height",
+	}
+
+	//send raw transaction
+	ContractAddrFlag = cli.StringFlag{
+		Name: "addr",
+	}
+	TransactionFromFlag = cli.StringFlag{
+		Name: "from",
+	}
+	TransactionToFlag = cli.StringFlag{
+		Name: "to",
+	}
+	TransactionValueFlag = cli.Int64Flag{
+		Name: "value",
+	}
+	UserPasswordFlag = cli.StringFlag{
+		Name: "password",
+	}
+
+	DebugLevelFlag = cli.UintFlag{
+		Name: "level",
+	}
+
+	ConsensusLevelFlag = cli.UintFlag{
+		Name: "on",
+	}
+
+	//contract deploy
+	ContractVmTypeFlag = cli.UintFlag{
+		Name: "type",
+	}
+
+	ContractStorageFlag = cli.BoolFlag{
+		Name: "store",
+	}
+
+	ContractCodeFlag = cli.StringFlag{
+		Name: "code",
+	}
+
+	ContractNameFlag = cli.StringFlag{
+		Name: "name",
+	}
+
+	ContractVersionFlag = cli.StringFlag{
+		Name: "version",
+	}
+
+	ContractAuthorFlag = cli.StringFlag{
+		Name: "author",
+	}
+
+	ContractEmailFlag = cli.StringFlag{
+		Name: "email",
+	}
+
+	ContractDescFlag = cli.StringFlag{
+		Name: "desc",
+	}
+
+	ContractParamsFlag = cli.StringFlag{
+		Name: "params",
+	}
+
+	//contract invoke
+)
+
+func (self *DirectoryFlag) Set(value string) {
+	self.Value.Value = value
+}
+
+// Expands a file path
+// 1. replace tilde with users home dir
+// 2. expands embedded environment variables
+// 3. cleans the path, e.g. /a/b/../c -> /a/c
+// Note, it has limitations, e.g. ~someuser/tmp will not be expanded
+func expandPath(p string) string {
+	if strings.HasPrefix(p, "~/") || strings.HasPrefix(p, "~\\") {
+		if home := os.Getenv("HOME"); home != "" {
+			p = home + p[1:]
+		}
+	}
+	return path.Clean(os.ExpandEnv(p))
+}
+
+func (self *DirectoryString) Set(value string) error {
+	self.Value = expandPath(value)
+	return nil
+}
+
+// DefaultDataDir is the default data directory to use for the databases and other
+// persistence requirements.
+func DefaultDataDir() string {
+	// Try to place the data folder in the user's home dir
+	home := os.Getenv("HOME")
+	if home != "" {
+		if runtime.GOOS == "darwin" {
+			return filepath.Join(home, "Library", "Ontology")
+		} else if runtime.GOOS == "windows" {
+			return filepath.Join(home, "AppData", "Roaming", "Ontology")
+		} else {
+			return filepath.Join(home, ".ontology")
+		}
+	}
+	// As we cannot guess a stable location, return empty and handle later
+	return ""
+}
+
+// MigrateFlags sets the global flag from a local flag when it's set.
+// This is a temporary function used for migrating old command/flags to the
+// new format.
+//
+// e.g. geth account new --keystore /tmp/mykeystore --lightkdf
+//
+// is equivalent after calling this method with:
+//
+// geth --keystore /tmp/mykeystore --lightkdf account new
+//
+// This allows the use of the existing configuration functionality.
+// When all flags are migrated this function can be removed and the existing
+// configuration functionality must be changed that is uses local flags
+func MigrateFlags(action func(ctx *cli.Context) error) func(*cli.Context) error {
+	return func(ctx *cli.Context) error {
+		for _, name := range ctx.FlagNames() {
+			if ctx.IsSet(name) {
+				ctx.GlobalSet(name, ctx.String(name))
+			}
+		}
+		return action(ctx)
+	}
+}
+
+// MakeDataDir retrieves the currently requested data directory, terminating
+// if none (or the empty string) is specified. If the node is starting a testnet,
+// the a subdirectory of the specified datadir will be used.
+func MakeDataDir(ctx *cli.Context) string {
+	if path := ctx.GlobalString(DataDirFlag.Name); path != "" {
+		if ctx.GlobalBool(TestnetFlag.Name) {
+			return filepath.Join(path, "testnet")
+		}
+		if ctx.GlobalBool(RinkebyFlag.Name) {
+			return filepath.Join(path, "rinkeby")
+		}
+		return path
+	}
+	fmt.Println("Cannot determine default data directory, please set manually (--datadir)")
+	return ""
+}
+
+func absolutePath(Datadir string, filename string) string {
+	if filepath.IsAbs(filename) {
+		return filename
+	}
+	return filepath.Join(Datadir, filename)
+}
+
+// MakeConsolePreloads retrieves the absolute paths for the console JavaScript
+// scripts to preload before starting.
+func MakeConsolePreloads(ctx *cli.Context) []string {
+	// Skip preloading if there's nothing to preload
+	if ctx.GlobalString(PreloadJSFlag.Name) == "" {
+		return nil
+	}
+	// Otherwise resolve absolute paths and return them
+	preloads := []string{}
+
+	assets := ctx.GlobalString(JSpathFlag.Name)
+	for _, file := range strings.Split(ctx.GlobalString(PreloadJSFlag.Name), ",") {
+		preloads = append(preloads, absolutePath(assets, strings.TrimSpace(file)))
+	}
+	return preloads
+}
+
+func eachName(longName string, fn func(string)) {
+	parts := strings.Split(longName, ",")
+	for _, name := range parts {
+		name = strings.Trim(name, " ")
+		fn(name)
+	}
+}
+
+func (self DirectoryFlag) GetName() string {
+	return self.Name
+}
+
+func (self *DirectoryString) String() string {
+	return self.Value
+}
+
+func prefixFor(name string) (prefix string) {
+	if len(name) == 1 {
+		prefix = "-"
+	} else {
+		prefix = "--"
+	}
+
+	return
+}
+
+func prefixedNames(fullName string) (prefixed string) {
+	parts := strings.Split(fullName, ",")
+	for i, name := range parts {
+		name = strings.Trim(name, " ")
+		prefixed += prefixFor(name) + name
+		if i < len(parts)-1 {
+			prefixed += ", "
+		}
+	}
+	return
+}
+
+func (self DirectoryFlag) String() string {
+	fmtString := "%s %v\t%v"
+	if len(self.Value.Value) > 0 {
+		fmtString = "%s \"%v\"\t%v"
+	}
+	return fmt.Sprintf(fmtString, prefixedNames(self.Name), self.Value.Value, self.Usage)
+}
+
+// called by cli library, grabs variable from environment (if in env)
+// and adds variable to flag set for parsing.
+func (self DirectoryFlag) Apply(set *flag.FlagSet) {
+	eachName(self.Name, func(name string) {
+		set.Var(&self.Value, self.Name, self.Usage)
+	})
+}
