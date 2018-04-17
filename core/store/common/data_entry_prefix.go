521beb23a4dfa61f2cd6069f8565767beb2a0939
gasby88 <wangbing@onchain.com>
2018-04-17 14:17:04 +0800

    add testcase in store (#22)
    signed-off-by:wangbing@onchain.com

--- core/store/common/data_entry_prefix.go
+++ core/store/common/data_entry_prefix.go
@@ -23,24 +23,24 @@ type DataEntryPrefix byte
 
 const (
 	// DATA
-	DATA_BLOCK       DataEntryPrefix = 0x00
-	DATA_HEADER                      = 0x01
-	DATA_TRANSACTION                 = 0x02
+	DATA_BLOCK       DataEntryPrefix = 0x00 //Block height => block hash key prefix
+	DATA_HEADER                      = 0x01 //Block hash => block hash key prefix
+	DATA_TRANSACTION                 = 0x02 //Transction hash = > transaction key prefix
 
 	// Transaction
-	ST_BOOKKEEPER DataEntryPrefix = 0x03
-	ST_CONTRACT   DataEntryPrefix = 0x04
-	ST_STORAGE    DataEntryPrefix = 0x05
-	ST_VALIDATOR  DataEntryPrefix = 0x07
-	ST_VOTE       DataEntryPrefix = 0x08
+	ST_BOOKKEEPER DataEntryPrefix = 0x03 //BookKeeper state key prefix
+	ST_CONTRACT   DataEntryPrefix = 0x04 //Smart contract state key prefix
+	ST_STORAGE    DataEntryPrefix = 0x05 //Smart contract storage key prefix
+	ST_VALIDATOR  DataEntryPrefix = 0x07 //no use
+	ST_VOTE       DataEntryPrefix = 0x08 //Vote state key prefix
 
-	IX_HEADER_HASH_LIST DataEntryPrefix = 0x09
+	IX_HEADER_HASH_LIST DataEntryPrefix = 0x09 //Block height => block hash key prefix
 
 	//SYSTEM
-	SYS_CURRENT_BLOCK      DataEntryPrefix = 0x10
-	SYS_VERSION            DataEntryPrefix = 0x11
-	SYS_CURRENT_STATE_ROOT DataEntryPrefix = 0x12
-	SYS_BLOCK_MERKLE_TREE  DataEntryPrefix = 0x13
+	SYS_CURRENT_BLOCK      DataEntryPrefix = 0x10 //Current block key prefix
+	SYS_VERSION            DataEntryPrefix = 0x11 //Store version key prefix
+	SYS_CURRENT_STATE_ROOT DataEntryPrefix = 0x12 //no use
+	SYS_BLOCK_MERKLE_TREE  DataEntryPrefix = 0x13 // Block merkle tree root key prefix
 
-	EVENT_NOTIFY DataEntryPrefix = 0x14
+	EVENT_NOTIFY DataEntryPrefix = 0x14 //Event notify key prefix
 )
---
+++ core/store/statestore/state_batch_test.go
@@ -0,0 +1,168 @@
+package statestore
+
+import (
+	"fmt"
+	"github.com/ontio/ontology/core/states"
+	com "github.com/ontio/ontology/core/store/common"
+	"github.com/ontio/ontology/core/store/leveldbstore"
+	"os"
+	"testing"
+	"github.com/syndtr/goleveldb/leveldb"
+)
+
+var (
+	testBatch   *StateBatch
+	testLevelDB *leveldbstore.LevelDBStore
+)
+
+func TestMain(m *testing.M) {
+	dbFile := "test"
+	var err error
+	testLevelDB, err = leveldbstore.NewLevelDBStore(dbFile)
+	if err != nil {
+		fmt.Printf("NewLevelDBStore:%s error:%s", dbFile, err)
+		return
+	}
+	testBatch = NewStateStoreBatch(NewMemDatabase(), testLevelDB)
+	m.Run()
+	testLevelDB.Close()
+	os.RemoveAll(dbFile)
+	os.RemoveAll("ActorLog")
+}
+
+func TestStateBatch_TryGetOrAdd(t *testing.T) {
+	prefix := com.ST_STORAGE
+	key := []byte("foo")
+	value := &states.StorageItem{Value: []byte("bar")}
+
+	err := testBatch.TryGetOrAdd(prefix, key, value)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd TryGetOrAdd error:%s", err)
+		return
+	}
+
+	v , err := testBatch.TryGet(prefix, key)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd TryGet error:%s", err)
+		return
+	}
+
+	storeItem := v.Value.(*states.StorageItem)
+	if string(storeItem.Value) != string(value.Value){
+		t.Errorf("TestStateBatch_TryGetOrAdd value:%s != %s", storeItem.Value, value.Value)
+		return
+	}
+}
+
+func TestStateBatch_TryAdd(t *testing.T) {
+	prefix := com.ST_STORAGE
+	key := []byte("foo1")
+	value := &states.StorageItem{Value: []byte("bar1")}
+
+	err := testBatch.TryGetOrAdd(prefix, key, value)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd TryGetOrAdd error:%s", err)
+		return
+	}
+
+	v , err := testBatch.TryGet(prefix, key)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd TryGet error:%s", err)
+		return
+	}
+
+	storeItem := v.Value.(*states.StorageItem)
+	if string(storeItem.Value) != string(value.Value){
+		t.Errorf("TestStateBatch_TryGetOrAdd value:%s != %s", storeItem.Value, value.Value)
+		return
+	}
+}
+
+func TestStateBatch_TryGetAndChange(t *testing.T) {
+	prefix := com.ST_STORAGE
+	key := []byte("foo")
+	value := &states.StorageItem{Value: []byte("bar")}
+
+	err := testBatch.TryGetOrAdd(prefix, key, value)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd TryGetOrAdd error:%s", err)
+		return
+	}
+
+	key1 := []byte("foo1")
+	value1 := &states.StorageItem{Value: []byte("bar1")}
+
+	err = testBatch.TryGetOrAdd(prefix, key1, value1)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd TryGetOrAdd error:%s", err)
+		return
+	}
+
+	testBatch.TryDelete(prefix, key)
+
+	v, err := testBatch.TryGetAndChange(prefix, key)
+	if err != nil {
+		t.Errorf("TryGetAndChange error:%s", err)
+		return
+	}
+
+	if v != nil {
+		t.Errorf("TryGetAndChange error key:%s should nil", key)
+		return
+	}
+
+	v1, err := testBatch.TryGetAndChange(prefix, key1)
+	if err != nil {
+		t.Errorf("TryGetAndChange error:%s", err)
+		return
+	}
+
+	storeItem := v1.(*states.StorageItem)
+	if string(storeItem.Value) != string(value1.Value){
+		t.Errorf("TestStateBatch_TryGetOrAdd value:%s != %s", storeItem.Value, value.Value)
+		return
+	}
+}
+
+func TestStateBatch_CommitTo(t *testing.T) {
+	prefix := com.ST_STORAGE
+	key := []byte("foo1")
+	value := &states.StorageItem{Value: []byte("bar1")}
+
+	err := testBatch.TryGetOrAdd(prefix, key, value)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd TryGetOrAdd error:%s", err)
+		return
+	}
+
+	testLevelDB.NewBatch()
+	err = testBatch.CommitTo()
+	if err != nil {
+		t.Errorf("CommitTo error:%s", err)
+		return
+	}
+
+	err = testLevelDB.BatchCommit()
+	if err != nil {
+		t.Errorf("BatchCommit error:%s", err)
+		return
+	}
+
+	data ,err := testLevelDB.Get(append([]byte{byte(prefix)}, key...))
+	if err != nil && err != leveldb.ErrNotFound {
+		t.Errorf("testLevelDB.Get error:%s", err)
+		return
+	}
+
+	item, err := getStateObject(prefix, data)
+	if err != nil {
+		t.Errorf("TestStateBatch_TryGetOrAdd getStateObject eror:%s", err)
+		return
+	}
+
+	v := item.(*states.StorageItem)
+	if string(v.Value) != string(value.Value) {
+		t.Errorf("TestStateBatch_TryGetOrAdd value:%s != %s", v.Value, value.Value)
+		return
+	}
+}
