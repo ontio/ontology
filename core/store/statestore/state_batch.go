f60615aa1232affa8eb21cc9f87d8eca989ca220
tanyuan <1067598718@qq.com>
2018-04-25 19:29:34 +0800

    Update state_batch.go

--- core/store/statestore/state_batch.go
+++ core/store/statestore/state_batch.go
@@ -5,7 +5,7 @@
  * The ontology is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at y:wqour option) any later version.
+ * (at your option) any later version.
  *
  * The ontology is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
