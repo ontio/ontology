ed7d067c8043713df847da68dbcaddc8d94f3a4e
JunXi Xie <xiemylogos@gmail.com>
2018-05-04 11:46:54 +0800

    vbft update peer pool after new consensus network settled

--- consensus/vbft/service.go
+++ consensus/vbft/service.go
@@ -211,6 +211,13 @@ func (self *Server) handleBlockPersistCompleted(block *types.Block) {
 
 	self.incrValidator.AddBlock(block)
 
+	if self.checkNeedUpdateChainConfig() || self.checkForceUpdateChainConfig() {
+		err := self.updateChainConfig()
+		if err != nil {
+			log.Errorf("updateChainConfig failed:%s", err)
+		}
+	}
+
 	if uint64(block.Header.Height) > self.completedBlockNum {
 		self.completedBlockNum = uint64(block.Header.Height)
 	} else {
@@ -326,10 +333,20 @@ func (self *Server) updateChainConfig() error {
 		}
 	}
 
+	if self.Index == math.MaxUint32 {
+		id, _ := vconfig.PubkeyID(self.account.PublicKey)
+		index, present := self.peerPool.GetPeerIndex(id)
+		if present {
+			self.Index = index
+		}
+	}
+
 	for id, index := range self.peerPool.IDMap {
 		_, present := peermap[id]
 		if !present {
-			if index != self.Index {
+			if index == self.Index {
+				self.Index = math.MaxUint32
+			} else {
 				log.Info("updateChainConfig remove consensus")
 				if C, present := self.msgRecvC[index]; present {
 					C <- nil
@@ -388,8 +405,15 @@ func (self *Server) initialize() error {
 		log.Infof("added peer: %s", p.ID.String())
 	}
 
+	//index equal math.MaxUint32  is noconsensus node
 	id, _ := vconfig.PubkeyID(self.account.PublicKey)
-	self.Index, _ = self.peerPool.GetPeerIndex(id)
+	index, present := self.peerPool.GetPeerIndex(id)
+	if present {
+		self.Index = index
+	} else {
+		self.Index = math.MaxUint32
+	}
+
 	self.sub.Subscribe(message.TOPIC_SAVE_BLOCK_COMPLETE)
 	go self.syncer.run()
 	go self.stateMgr.run()
@@ -1857,6 +1881,9 @@ func (self *Server) msgSendLoop() {
 	for {
 		select {
 		case evt := <-self.msgSendC:
+			if self.Index == math.MaxUint32 {
+				continue
+			}
 			payload, err := SerializeVbftMsg(evt.Msg)
 			if err != nil {
 				log.Errorf("server %d failed to serialized msg (type: %d): %s", self.Index, evt.Msg.Type(), err)
@@ -1894,6 +1921,22 @@ func (self *Server) createBookkeepingTransaction(nonce uint64, fee uint64) *type
 		Payload:    bookKeepingPayload,
 		Attributes: []*types.TxAttribute{},
 	}
+}
+
+//createfeeSplitTransaction invoke fee native contract EXECUTE_SPLIT
+func (self *Server) createfeeSplitTransaction() *types.Transaction {
+	init := states.Contract{
+		Address: genesis.GovernanceContractAddress,
+		Method:  "executeSplit",
+	}
+	bf := new(bytes.Buffer)
+	init.Serialize(bf)
+	vmCode := stypes.VmCode{
+		VmType: stypes.Native,
+		Code:   bf.Bytes(),
+	}
+	tx := utils.NewInvokeTransaction(vmCode)
+	return tx
 }
 
 //creategovernaceTransaction invoke governance native contract commit_pos
@@ -1961,8 +2004,8 @@ func (self *Server) makeProposal(blkNum uint64, forEmpty bool) error {
 		if err != nil {
 			log.Errorf("updateChainConfig failed:%s", err)
 		} else {
-			//add transaction invoke governance native,commit_pos contract
-			txs = append(txs, self.creategovernaceTransaction())
+			//add transaction invoke governance native,executeSplit、commit_pos contract
+			txs = append(txs, self.createfeeSplitTransaction(), self.creategovernaceTransaction())
 		}
 	}
 
