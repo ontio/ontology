deb97cb4154031e3c82fe1a7ac6078f52c83a82b
Doresimon <doresimon@163.com>
2018-05-08 17:59:30 +0800

    fix a help info of <account add -n> option.
    Signed-off-by: Halo Guan <guanjixing@onchain.com>

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -160,7 +160,8 @@ var (
 	}
 	AccountQuantityFlag = cli.UintFlag{
 		Name:  "number,n",
-		Usage: "Specifies the `<quantity>` of account to create, default is 1.",
+		Value: 1,
+		Usage: "Specifies the `<quantity>` of account to create.",
 	}
 	AccountFileFlag = cli.StringFlag{
 		Name:  "file,f",
@@ -223,7 +224,7 @@ var (
 		Value: "",
 	}
 	ContractParamsFlag = cli.StringFlag{
-		Name: "params",
+		Name:  "params",
 		Usage: "Invoke contract parameters list. use comma ',' to split params, and must add type prefix to params.0:bytearray(hexstring), 1:string, 2:integer, 3:boolean,For example: 1:foo,2:0,3:true;If parameter is an object array, enclose array with '[]', For example:  1:foo,[2:0,3:true]",
 	}
 
