69f1bffe2c3988d9f0cb996a02c9a7201eb9968e
tanyuan <1067598718@qq.com>
2018-05-09 17:06:51 +0800

    specification name

--- core/store/ledgerstore/state_store.go
+++ core/store/ledgerstore/state_store.go
@@ -34,7 +34,7 @@ import (
 )
 
 var (
-	BookerKeeper = []byte("Booker-Keeper") //BookerKeeper store key
+	BOOKKEEPER = []byte("Bookkeeper") //Bookkeeper store key
 )
 
 //StateStore saving the data of ledger states. Like balance of account, and the execution result of smart contract
@@ -307,9 +307,9 @@ func (self *StateStore) getCurrentBlockKey() []byte {
 }
 
 func (self *StateStore) getBookkeeperKey() ([]byte, error) {
-	key := make([]byte, 1+len(BookerKeeper))
+	key := make([]byte, 1+len(BOOKKEEPER))
 	key[0] = byte(scom.ST_BOOKKEEPER)
-	copy(key[1:], []byte(BookerKeeper))
+	copy(key[1:], []byte(BOOKKEEPER))
 	return key, nil
 }
 
