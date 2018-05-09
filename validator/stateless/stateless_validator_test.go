ef2e521a3bb84e2e5166005a18dfbfc0cb43df08
tanyuan <1067598718@qq.com>
2018-05-09 16:34:50 +0800

    go fmt

--- validator/stateless/stateless_validator_test.go
+++ validator/stateless/stateless_validator_test.go
@@ -39,7 +39,7 @@ func TestStatelessValidator(t *testing.T) {
 	tx := utils.NewDeployTransaction(code, "test", "1", "author", "author@123.com", "test desp", false)
 
 	tx.Payer = acc.Address
-	
+
 	signTransaction(acc, tx)
 
 	validator := &validator{id: "test"}
