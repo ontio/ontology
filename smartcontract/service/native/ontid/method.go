a349536b10a8a5074a1c3ef9a6c920f549c4d7c8
Alver Lyu <AlverLyu@gmail.com>
2018-05-15 18:39:26 +0800

    fix bug in ontid.addRecovery

--- smartcontract/service/native/ontid/method.go
+++ smartcontract/service/native/ontid/method.go
@@ -1,20 +1,3 @@
-/*
- * Copyright (C) 2018 The ontology Authors
- * This file is part of The ontology library.
- *
- * The ontology is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The ontology is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
- */
 package ontid
 
 import (
@@ -290,13 +273,12 @@ func addRecovery(srvc *native.NativeService) ([]byte, error) {
 	if !checkIDExistence(srvc, key) {
 		return utils.BYTE_FALSE, errors.New("add recovery failed: ID not registered")
 	}
-
 	if !isOwner(srvc, key, arg2) {
 		return utils.BYTE_FALSE, errors.New("add recovery failed: not authorized")
 	}
 
 	re, err := getRecovery(srvc, key)
-	if err != nil && len(re) > 0 {
+	if err == nil && len(re) > 0 {
 		return utils.BYTE_FALSE, errors.New("add recovery failed: already set recovery")
 	}
 
