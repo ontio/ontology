adfd85b7e0f1b00cc93dc709de84a74105ee8219
Alver Lyu <AlverLyu@gmail.com>
2018-05-16 18:44:39 +0800

    remove addAttribute method

--- smartcontract/service/native/ontid/init.go
+++ smartcontract/service/native/ontid/init.go
@@ -33,7 +33,6 @@ func RegisterIDContract(srvc *native.NativeService) {
 	srvc.Register("addRecovery", addRecovery)
 	srvc.Register("changeRecovery", changeRecovery)
 	srvc.Register("regIDWithAttributes", regIdWithAttributes)
-	srvc.Register("addAttribute", addAttribute)
 	srvc.Register("addAttributes", addAttributes)
 	srvc.Register("removeAttribute", removeAttribute)
 	srvc.Register("verifySignature", verifySignature)
