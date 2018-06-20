ddf2c815ac4386483dff3e772e4efb27bd328d6e
tanyuan <1067598718@qq.com>
2018-06-20 10:39:25 +0800

    add safe math calculate

---
+++ common/safeMath.go
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package common
+
+import "math"
+
+const (
+	MAX_UINT64 = math.MaxUint64
+)
+
+func SafeSub(x, y uint64) (uint64, bool) {
+	return x - y, x < y
+}
+
+func SafeAdd(x, y uint64) (uint64, bool) {
+	return x + y, y > MAX_UINT64-x
+}
+
+func SafeMul(x, y uint64) (uint64, bool) {
+	if x == 0 || y == 0 {
+		return 0, false
+	}
+	return x * y, y > MAX_UINT64/x
+}
