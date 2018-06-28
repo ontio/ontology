d2dca55d6044318b83a496a3e2b27cb87a96add1
zhoupw@gmail.com <zhoupw@gmail.com>
2018-06-28 17:32:23 +0800

    add license head

--- core/store/ledgerstore/tx_handler_test.go
+++ core/store/ledgerstore/tx_handler_test.go
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ledgerstore
 
 import (
@@ -26,24 +44,5 @@ func TestSyncMapRange(t *testing.T) {
 	})
 
 	fmt.Println(cnt)
-
-}
-
-func TestSyncMapRW(t *testing.T) {
-	var wg sync.WaitGroup
-
-	wg.Add(1000)
-	m := &sync.Map{}
-
-	m.Store("key", 1)
-
-	for i := 0; i < 1000; i++ {
-		go writeAndRead(t, m)
-	}
-
-}
-func writeAndRead(t *testing.T, m *sync.Map) {
-	v, _ := m.Load("key")
-	m.Store("key", v.(int)+1)
 
 }
