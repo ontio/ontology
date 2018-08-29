59cc28d9bf1f8d56333ff30e4107a6aa149aedcb
laizy <laizy@users.noreply.github.com>
2018-08-29 15:19:38 +0800

    fix Addr serialization (#630)
    Signed-off-by: laizy <laizhichao@onchain.com>

--- p2pserver/message/types/address.go
+++ p2pserver/message/types/address.go
@@ -39,6 +39,7 @@ func (this Addr) Serialization(sink *common.ZeroCopySink) error {
 		sink.WriteUint64(addr.Services)
 		sink.WriteBytes(addr.IpAddr[:])
 		sink.WriteUint16(addr.Port)
+		sink.WriteUint16(addr.ConsensusPort)
 		sink.WriteUint64(addr.ID)
 	}
 
@@ -62,6 +63,7 @@ func (this *Addr) Deserialization(source *common.ZeroCopySource) error {
 		buf, _ := source.NextBytes(uint64(len(addr.IpAddr[:])))
 		copy(addr.IpAddr[:], buf)
 		addr.Port, eof = source.NextUint16()
+		addr.ConsensusPort, eof = source.NextUint16()
 		addr.ID, eof = source.NextUint64()
 		if eof {
 			return io.ErrUnexpectedEOF
