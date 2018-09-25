62968f211f9f29ccd12c627fb9c98d6b0e241c0b
laizy <laizhichao@onchain.com>
2018-09-25 18:13:14 +0800

    change governance version view and height

--- smartcontract/service/native/governance/governance.go
+++ smartcontract/service/native/governance/governance.go
@@ -97,8 +97,8 @@ const (
 
 	//global
 	PRECISE           = 1000000
-	NEW_VERSION_BLOCK = 357700
-	NEW_VERSION_VIEW  = 5
+	NEW_VERSION_BLOCK = 414100
+	NEW_VERSION_VIEW  = 6
 )
 
 // candidate fee must >= 1 ONG
