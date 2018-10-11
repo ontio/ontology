c6dfe751778a20f5223efd75e93d756755564780
tanZiWen <1067598718@qq.com>
2018-10-11 19:18:53 +0800

    fix rollback code

--- core/store/ledgerstore/ledger_store.go
+++ core/store/ledgerstore/ledger_store.go
@@ -26,7 +26,7 @@ import (
 	"sort"
 	"strings"
 	"sync"
-
+	"time"
 	"strconv"
 
 	"github.com/ontio/ontology-crypto/keypair"
@@ -846,14 +846,10 @@ func (this *LedgerStoreImp) GetEventNotifyByBlock(height uint32) ([]*event.Execu
 func (this *LedgerStoreImp) PreExecuteContract(tx *types.Transaction) (*sstate.PreExecResult, error) {
 	height := this.GetCurrentBlockHeight()
 	stf := &sstate.PreExecResult{State: event.CONTRACT_STATE_FAIL, Gas: neovm.MIN_TRANSACTION_GAS, Result: nil}
-	header, err := this.GetHeaderByHeight(height)
-	if err != nil {
-		return stf, err
-	}
 
 	config := &smartcontract.Config{
-		Time:       header.Timestamp,
-		Height:     header.Height,
+		Time:       uint32(time.Now().Unix()),
+		Height:     height + 1,
 		Tx:         tx,
 		RandomHash: this.GetBlockHash(height),
 	}
