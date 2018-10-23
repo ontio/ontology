af85eac5c7f4660c694a0231645495fff2b88a0b
laizy <laizhichao@onchain.com>
2018-10-23 18:49:37 +0800

    implement executor

--- vm/neovm/params.go
+++ vm/neovm/params.go
@@ -19,7 +19,7 @@
 package neovm
 
 const (
-	STACK_LIMIT               uint32 = 2 * 1024
+	STACK_LIMIT                = 2 * 1024
 	MAX_INVOCATION_STACK_SIZE        = 1024
 	MAX_SIZE_FOR_BIGINTEGER          = 32
 	MAX_BYTEARRAY_SIZE        uint32 = 1024 * 1024
