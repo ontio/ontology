d8caad1513cc300e352989584058410aaa3ff0c3
laizy <laizhichao@onchain.com>
2018-10-26 11:19:57 +0800

    implement array opcode

--- vm/neovm/types/struct_value.go
+++ vm/neovm/types/struct_value.go
@@ -2,9 +2,17 @@ package types
 
 // struct value is value type
 type StructValue struct {
-	data []VmValue
+	Data []VmValue
+}
+
+func NewStructValue() StructValue {
+	return StructValue{Data: make([]VmValue, 0, initArraySize)}
 }
 
 func (self *StructValue) Append(item VmValue) StructValue {
-	return StructValue{data: append(self.data, item)}
+	return StructValue{Data: append(self.Data, item)}
+}
+
+func (self *StructValue)Len() int64 {
+	return int64(len(self.Data))
 }
