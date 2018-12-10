b7f6557c1753175e5069e197a5b6f924acaa4509
laizy <laizhichao@onchain.com>
2018-12-10 10:36:46 +0800

    fix

--- core/types/transaction.go
+++ core/types/transaction.go
@@ -108,7 +108,7 @@ func (tx *Transaction) Deserialization(source *common.ZeroCopySource) error {
 	pend := source.Pos()
 	lenAll := pend - pstart
 	if lenAll > MAX_TX_SIZE {
-		return errors.New("execced max transaction size")
+		return fmt.Errorf("execced max transaction size:%d", lenAll)
 	}
 	source.BackUp(lenAll)
 	tx.Raw, _ = source.NextBytes(lenAll)
