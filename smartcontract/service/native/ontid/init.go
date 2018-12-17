fe1e86567605f06c959edc5bcc86abc9cfdeae2b
Alver Lyu <AlverLyu@gmail.com>
2018-12-17 17:14:10 +0800

    remove unused ontid interfaces

--- smartcontract/service/native/ontid/init.go
+++ smartcontract/service/native/ontid/init.go
@@ -27,14 +27,6 @@ func Init() {
 }
 
 func RegisterIDContract(srvc *native.NativeService) {
-	srvc.Register("regIDWithPublicKey", regIdWithPublicKey)
-	srvc.Register("addKey", addKey)
-	srvc.Register("removeKey", removeKey)
-	srvc.Register("addRecovery", addRecovery)
-	srvc.Register("changeRecovery", changeRecovery)
-	srvc.Register("regIDWithAttributes", regIdWithAttributes)
-	srvc.Register("addAttributes", addAttributes)
-	srvc.Register("removeAttribute", removeAttribute)
 	srvc.Register("verifySignature", verifySignature)
 	srvc.Register("getPublicKeys", GetPublicKeys)
 	srvc.Register("getKeyState", GetKeyState)
