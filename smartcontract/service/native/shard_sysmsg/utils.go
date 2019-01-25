15f0b07eb75bd6a8b16859b604e9a9369401f65a
Edmond-Honglei-Cong <conghonglei@gmail.com>
2019-01-25 17:50:42 +0800

    add shardID to txn

---
+++ smartcontract/service/native/shard_sysmsg/utils.go
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2019 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package shardsysmsg
+
+import (
+	"fmt"
+
+	"github.com/ontio/ontology/smartcontract/service/native/ont"
+	"github.com/ontio/ontology/smartcontract/service/native"
+	"github.com/ontio/ontology/common"
+)
+
+func appCallTransfer(native *native.NativeService, contract common.Address, from common.Address, to common.Address, amount uint64) error {
+	var sts []ont.State
+	sts = append(sts, ont.State{
+		From:  from,
+		To:    to,
+		Value: amount,
+	})
+	transfers := ont.Transfers{
+		States: sts,
+	}
+	sink := common.NewZeroCopySink(nil)
+	transfers.Serialization(sink)
+
+	if _, err := native.NativeCall(contract, "transfer", sink.Bytes()); err != nil {
+		return fmt.Errorf("appCallTransfer, appCall error: %v", err)
+	}
+	return nil
+}
