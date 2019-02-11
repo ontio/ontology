1ea9080d2f6e8ee0ec21bfc288d3c738e71af555
Edmond-Honglei-Cong <conghonglei@gmail.com>
2019-02-11 16:39:20 +0800

    add license headers

--- core/chainmgr/message/build-protos.sh
+++ core/chainmgr/message/build-protos.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # go get github.com/gogo/protobuf/protoc-gen-gogoslick
 # add protoc-gen-gogoslick to your $PATH
 protoc -I=. -I=$GOPATH/src --gogoslick_out=plugins=grpc:. protos.proto
