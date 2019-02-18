b15cc107e4e9375ec1c24185bd2d516c6a14fd0e
Edmond-Honglei-Cong <conghonglei@gmail.com>
2019-02-18 19:06:47 +0800

    add chainmgr message testcases

---
+++ core/chainmgr/message/message_test.go
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2019 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package message_test
+
+import (
+	"testing"
+	"github.com/ontio/ontology/core/chainmgr/message"
+)
+
+func TestShardHelloMsg(t *testing.T) {
+	hello := &message.ShardHelloMsg{100, 200}
+	helloBytes, err := message.EncodeShardMsg(hello)
+	if err != nil {
+		t.Fatalf("failed to encode hello: %s", err)
+	}
+	msg2, err := message.DecodeShardMsg(message.HELLO_MSG, helloBytes)
+	if err != nil {
+		t.Fatalf("failed to decode hello: %s", err)
+	}
+	hello2, ok := msg2.(*message.ShardHelloMsg)
+	if !ok {
+		t.Fatalf("invalid hello msg type")
+	}
+	if hello.SourceShardID != hello2.SourceShardID {
+		t.Fatalf("hello: invalid source shard id")
+	}
+	if hello.TargetShardID != hello2.TargetShardID {
+		t.Fatalf("hello: invalid target shard id")
+	}
+}
+
+func TestShardBlockRspMsg(t *testing.T) {
+	blkHdr := newTestBlockHdr()
+	tx := newTestShardTx(t, 10, 1000)
+	rsp := &message.ShardBlockRspMsg{
+		FromShardID: 100,
+		Height: 200,
+		BlockHeader: blkHdr,
+		Txs: []*message.ShardBlockTx{tx},
+	}
+
+	msgBytes, err := message.EncodeShardMsg(rsp)
+	if err != nil {
+		t.Fatalf("failed to encode rsp msg: %s", err)
+	}
+
+	msg2, err := message.DecodeShardMsg(message.BLOCK_RSP_MSG, msgBytes)
+	if err != nil {
+		t.Fatalf("failed to decode hello: %s", err)
+	}
+	rsp2, ok := msg2.(*message.ShardBlockRspMsg)
+	if !ok {
+		t.Fatalf("invalid rsp msg type")
+	}
+
+	if rsp2.FromShardID != rsp.FromShardID {
+		t.Fatalf("invalid from shard id")
+	}
+	if rsp2.Height != rsp.Height {
+		t.Fatalf("invalid height")
+	}
+}
---
+++ core/chainmgr/message/msg_builder_test.go
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2019 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package message_test
+
+import (
+	"testing"
+	"github.com/ontio/ontology/core/chainmgr/message"
+	"github.com/ontio/ontology/account"
+	"bytes"
+)
+
+func TestNewCrossShardTxMsg(t *testing.T) {
+	acc := account.NewAccount("")
+	if acc == nil {
+		t.Fatalf("failed to new account")
+	}
+	payload := [][]byte{{1, 2,3, 4}}
+	tx, err := message.NewCrossShardTxMsg(acc, 100, 10, payload)
+	if err != nil {
+		t.Fatalf("failed to build cross shard tx: %s", err)
+	}
+
+	buf := new(bytes.Buffer)
+	if err := tx.Serialize(buf); err != nil {
+		t.Fatalf("failed to serialize cross shard tx: %s", err)
+	}
+}
