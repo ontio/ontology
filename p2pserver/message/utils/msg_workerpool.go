79aca2316b050027e19e72da25b4b56ee7a87f2a
likaioc <likai@onchain.com>
2019-02-22 18:55:53 +0800

    modify comment error

--- p2pserver/message/utils/msg_workerpool.go
+++ p2pserver/message/utils/msg_workerpool.go
@@ -33,7 +33,7 @@
   *furnished to do so, subject to the following conditions:
 
   *The above copyright notice and this permission notice shall be included in all
-  *copies or substantial portions of the Software.  *
+  *copies or substantial portions of the Software. 
   */
 
 package utils
