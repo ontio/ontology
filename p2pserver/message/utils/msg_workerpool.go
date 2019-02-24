3c80b186afed90d811cbc98ed9d011563ceaa650
likaioc <likai@onchain.com>
2019-02-24 12:04:35 +0800

    gofmt codes

--- p2pserver/message/utils/msg_workerpool.go
+++ p2pserver/message/utils/msg_workerpool.go
@@ -16,25 +16,25 @@
  * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
  */
 
- /*
-  * https://github.com/valyala/fasthttp
-  * The MIT License (MIT)
+/*
+ * https://github.com/valyala/fasthttp
+ * The MIT License (MIT)
 
-  *Copyright (c) 2015-present Aliaksandr Valialkin, VertaMedia
-  *Copyright (c) 2018-present Kirill Danshin
-  *Copyright (c) 2018-present Erik Dubbelboer
-  *Copyright (c) 2018-present FastHTTP Authors
+ *Copyright (c) 2015-present Aliaksandr Valialkin, VertaMedia
+ *Copyright (c) 2018-present Kirill Danshin
+ *Copyright (c) 2018-present Erik Dubbelboer
+ *Copyright (c) 2018-present FastHTTP Authors
 
-  *Permission is hereby granted, free of charge, to any person obtaining a copy
-  *of this software and associated documentation files (the "Software"), to deal
-  *in the Software without restriction, including without limitation the rights
-  *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  *copies of the Software, and to permit persons to whom the Software is
-  *furnished to do so, subject to the following conditions:
+ *Permission is hereby granted, free of charge, to any person obtaining a copy
+ *of this software and associated documentation files (the "Software"), to deal
+ *in the Software without restriction, including without limitation the rights
+ *to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *copies of the Software, and to permit persons to whom the Software is
+ *furnished to do so, subject to the following conditions:
 
-  *The above copyright notice and this permission notice shall be included in all
-  *copies or substantial portions of the Software. 
-  */
+ *The above copyright notice and this permission notice shall be included in all
+ *copies or substantial portions of the Software.
+ */
 
 package utils
 
