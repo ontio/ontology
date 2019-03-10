46400061c87a8c2884a19a2e380c2c980f8bd682
Alver Lyu <AlverLyu@gmail.com>
2019-03-10 13:35:27 +0800

    fix compile bugs

--- common/password/password_windows.go
+++ common/password/password_windows.go
@@ -1,20 +1,18 @@
-/*
- * Copyright (C) 2018 The ontology Authors
- * This file is part of The ontology library.
- *
- * The ontology is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The ontology is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright (C) 2018 The ontology Authors
+// This file is part of The ontology library.
+//
+// The ontology is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The ontology is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
 
 // +build windows
 
@@ -23,16 +21,15 @@ package password
 import (
 	"bytes"
 	"fmt"
-	"golang.org/x/crypto/ssh/terminal"
 	"os"
 
-	"github.com/howeyc/gopass"
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 // GetPassword gets password from user input
 func GetPassword() ([]byte, error) {
 	fmt.Printf("Password:")
-	passwd, err := terminal.ReadPassword(os.Stdin)
+	passwd, err := terminal.ReadPassword(int(os.Stdin.Fd()))
 	if err != nil {
 		return nil, err
 	}
@@ -42,7 +39,7 @@ func GetPassword() ([]byte, error) {
 // GetConfirmedPassword gets double confirmed password from user input
 func GetConfirmedPassword() ([]byte, error) {
 	fmt.Printf("Password:")
-	first, err := terminal.ReadPassword(os.Stdin)
+	first, err := terminal.ReadPassword(int(os.Stdin.Fd()))
 	if err != nil {
 		return nil, err
 	}
@@ -52,7 +49,7 @@ func GetConfirmedPassword() ([]byte, error) {
 	}
 
 	fmt.Printf("Re-enter Password:")
-	second, err := terminal.ReadPassword(os.Stdin)
+	second, err := terminal.ReadPassword(int(os.Stdin.Fd()))
 	if err != nil {
 		return nil, err
 	}
