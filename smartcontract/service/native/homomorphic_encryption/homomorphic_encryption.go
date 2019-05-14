4ba788c9944faaf354b0299739b2ba4c488d8fe1
galaxylegend2019 <galaxylegend2019@gmail.com>
2019-05-14 14:57:01 +0800

    fix: init add homomorphic encryption contract

---
+++ smartcontract/service/native/homomorphic_encryption/homomorphic_encryption.go
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2019 The DAD Authors
+ * This file is part of The DAD library.
+ *
+ * The DAD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The DAD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The DAD.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// homomorphic encryption contract:
+// User can use those API supported by this contract's to encrypt privacy data on chain
+// to pretect privacy data
+package homomorphicencryption
+
+import (
+	"github.com/ontio/ontology/smartcontract/service/native"
+	"github.com/ontio/ontology/smartcontract/service/native/utils"
+)
+
+const ()
+
+const (
+	//INIT_CONFIG function name
+	INIT_CONFIG = "initConfig"
+
+	//key prefix
+
+	//global
+)
+
+//InitHomomorphicEncryption init contract address
+func InitHomomorphicEncryption() {
+	native.Contracts[utils.HomomorphicEncryptionContractAddress] = RegisterHomomorphicEncryptionContract
+}
+
+//Register methods of HomomorphicEncryption contract
+func RegisterHomomorphicEncryptionContract(native *native.NativeService) {
+	native.Register(INIT_CONFIG, InitConfig)
+}
+
+//Init HomomorphicEncryption contract, include vbft config, global param and ontid admin.
+func InitConfig(native *native.NativeService) ([]byte, error) {
+}
