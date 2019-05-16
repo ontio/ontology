0fe7785b6732049edf71ad05fd9dec51916fc5df
chariotsfanatics <chariotsfanatics@hotmail.com>
2019-05-16 15:39:54 +0800

    fix: add function

--- smartcontract/service/native/homomorphic_encryption/bgn/ciphertext.go
+++ smartcontract/service/native/homomorphic_encryption/bgn/ciphertext.go
@@ -11,6 +11,11 @@ type Ciphertext struct {
 	L2           bool // whether ciphertext is level2
 }
 
+// Copy returns a copy of the given ciphertext
+func (ct *Ciphertext) Copy() *Ciphertext {
+	return &Ciphertext{ct.Coefficients, ct.Degree, ct.ScaleFactor, ct.L2}
+}
+
 // NewCiphertext generates a  new ciphertext...duh
 func NewCiphertext(coefficients []*pbc.Element, degree int, scaleFactor int, l2 bool) *Ciphertext {
 
