ee70a8827459cf02bfff3f5235e7bf341b1f6bd3
laizy <laizy@users.noreply.github.com>
2019-05-27 18:38:42 +0800

    fix get rawheader deserialization (#927)

--- core/types/header_test.go
+++ core/types/header_test.go
@@ -18,6 +18,7 @@
 package types
 
 import (
+	"crypto/rand"
 	"fmt"
 	"testing"
 
@@ -47,5 +48,38 @@ func TestHeader_Serialize(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, header.Height, h3.Height)
 	assert.Equal(t, bs, h3.Payload)
+
+	buf := common.SerializeToBytes(&h3)
+	assert.Equal(t, buf, bs)
+}
+
+func TestHeader_SerializeShardHeader(t *testing.T) {
+	header := Header{}
+	header.Version = common.VERSION_SUPPORT_SHARD
+	header.Height = 321
+	header.Bookkeepers = make([]keypair.PublicKey, 0)
+	var bookkeeper common.Address
+	_, _ = rand.Read(bookkeeper[:])
+	header.NextBookkeeper = bookkeeper
+	header.SigData = make([][]byte, 0)
+	sink := common.NewZeroCopySink(0)
+	header.Serialization(sink)
+	bs := sink.Bytes()
+
+	var h2 Header
+	source := common.NewZeroCopySource(bs)
+	err := h2.Deserialization(source)
+	assert.Nil(t, err)
+	assert.Equal(t, fmt.Sprint(header), fmt.Sprint(h2))
+
+	var h3 RawHeader
+	source = common.NewZeroCopySource(bs)
+	err = h3.Deserialization(source)
+	assert.Nil(t, err)
+	assert.Equal(t, header.Height, h3.Height)
+	assert.Equal(t, bs, h3.Payload)
+
+	buf := common.SerializeToBytes(&h3)
+	assert.Equal(t, buf, bs)
 
 }
