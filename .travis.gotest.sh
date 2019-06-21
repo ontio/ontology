ae34460e40bf8962eaf9537a2605b1f31f04c0e0
Edmond-Honglei-Cong <conghonglei@gmail.com>
2019-06-21 12:06:20 +0800

    update travis gotest script

--- .travis.gotest.sh
+++ .travis.gotest.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 # code from https://github.com/Seklfreak/Robyul2
 unset dirs files
-dirs=$(go list ./... | grep -v vendor/ | grep -v ontology$)
+dirs=$(go list ./... | grep -v vendor/ | grep -v testsuite | grep -v ontology$)
 set -x -e
 for d in $dirs
 do
-	go test -v $d
+        go test -v $d
 done
+
+testcases=('smartcontract' 'smartcontract/sys-contract' 'sync' 'p2p' 'xshard')
+for d in ${testcases[*]}
+do
+        rm -rf testsuite/Chain
+        go test -v githubcom/ontio/ontology/testsuite/$d
+done
+
