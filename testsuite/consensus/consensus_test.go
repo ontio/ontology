13dd604aa7427c95496cf4d93886d2dfca88dc3d
Edmond-Honglei-Cong <conghonglei@gmail.com>
2019-06-28 11:17:43 +0800

    add solo consensus test

--- testsuite/consensus/consensus_test.go
+++ testsuite/consensus/consensus_test.go
@@ -68,6 +68,35 @@ func StartMockerConsensus(t *testing.T, shardID common.ShardID, name string, src
 	return service
 }
 
+func StartMokerSoloConsensus(t *testing.T, shardID common.ShardID, name string, srcLgr *ledger.Ledger) consensus.ConsensusService {
+	shardName := chainmgr.GetShardName(shardID)
+
+	acc := TestCommon.GetAccount(shardName + "_" + name)
+	if acc == nil {
+		t.Fatalf("failed to get user account of shard %s", shardName)
+	}
+
+	lgr := TestCommon.CloneChain(t, name, srcLgr)
+	ledger.RemoveLedger(shardID)
+
+	txPool := TestCommon.NewTxnPool(t, name, shardID)
+	peer := TestCommon.NewPeer(lgr)
+	peer.Register()
+	p2pActor := TestCommon.NewP2PActor(t, name, peer)
+
+	txPool.Start(t)
+	p2pActor.Start(t)
+	peer.Start()
+
+	config.DefConfig.Genesis.SOLO.GenBlockTime = 1
+	service, err := consensus.NewConsensusService(consensus.CONSENSUS_SOLO, shardID, acc, txPool.GetPID(t), lgr, p2pActor.GetPID(t))
+	if err != nil {
+		t.Fatalf("start consensus: %s", err)
+	}
+	peer.SetConsensusPid(t, service.GetPID())
+	return service
+}
+
 func Test_NewConsensusService_7nodes(t *testing.T) {
 	shardID := common.NewShardIDUnchecked(config.DEFAULT_SHARD_ID)
 	events.Init()
@@ -103,6 +132,24 @@ func Test_NewConsensusService_7nodes(t *testing.T) {
 	}
 }
 
-func Test_MakeProposal(t *testing.T) {
+func Test_SoloConsensus(t *testing.T) {
+	shardID := common.NewShardIDUnchecked(config.DEFAULT_SHARD_ID)
+	events.Init()
+	xshard.InitCrossShardPool(shardID, 100)
 
+	// . create template chain
+	TestCommon.CreateChain(t, "src", shardID, 0)
+	lgr := ledger.GetShardLedger(shardID)
+	ledger.RemoveLedger(shardID)
+
+	name := "peerOwner0"
+	s := StartMokerSoloConsensus(t, shardID, name, lgr)
+	s.Start()
+
+	time.Sleep(15)
+
+	if lgr.GetCurrentBlockHeight() == 0 {
+		t.Fatalf("solo block height: %d", lgr.GetCurrentBlockHeight())
+	}
+	log.Infof("solo block height %d", lgr.GetCurrentBlockHeight())
 }
