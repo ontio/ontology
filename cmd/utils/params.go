3d90cb8302e48d8060d18aa7daada5e6d4893897
Zhou Peiwen <zhoupw@gmail.com>
2019-08-22 16:45:13 +0800

    support address type in sigsvr (#1058)

--- cmd/utils/params.go
+++ cmd/utils/params.go
@@ -21,6 +21,7 @@ package utils
 import (
 	"encoding/hex"
 	"fmt"
+	"github.com/ontio/ontology/common"
 	"reflect"
 	"strconv"
 	"strings"
@@ -34,6 +35,7 @@ const (
 	PARAM_TYPE_STRING     = "string"
 	PARAM_TYPE_INTEGER    = "int"
 	PARAM_TYPE_BOOLEAN    = "bool"
+	PARAM_TYPE_ADDRESS    = "address"
 	PARAM_LEFT_BRACKET    = "["
 	PARAM_RIGHT_BRACKET   = "]"
 	PARAM_ESC_CHAR        = `/`
@@ -194,6 +196,12 @@ func parseRawParamValue(pType string, pValue string) (interface{}, error) {
 		default:
 			return nil, fmt.Errorf("parse boolean param:%s failed", pValue)
 		}
+	case PARAM_TYPE_ADDRESS:
+		address, err := common.AddressFromBase58(pValue)
+		if err != nil {
+			return nil, fmt.Errorf("parse address param:%s failed", pValue)
+		}
+		return address[:], nil
 	default:
 		return nil, fmt.Errorf("unspport param type:%s", pType)
 	}
