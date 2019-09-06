f5ce2b223f8aef9ebb1a2448810081bfe0542362
laizy <laizhichao@onchain.com>
2019-09-06 14:30:28 +0800

    add jsvm run in wasm vm

--- wasmtest/contracts-rust/travis.build.sh
+++ wasmtest/contracts-rust/travis.build.sh
@@ -2,9 +2,14 @@
 set -e
 set -x
 
-RUSTFLAGS="-C link-arg=-zstack-size=32768" cargo build --release --target=wasm32-unknown-unknown
+rm -rf js-vm && git clone --depth=1 https://github.com/laizy/ontio-js-vm js-vm
+cd js-vm
+RUSTFLAGS="-C link-arg=-zstack-size=32768" cargo build --lib --release --target=wasm32-unknown-unknown
+cd ..
+mv ./target/wasm32-unknown-unknown/release/boa.wasm ../testwasmdata/jsvm.wasm
 
 cd helloworld
+RUSTFLAGS="-C link-arg=-zstack-size=32768" cargo build --release --target=wasm32-unknown-unknown
 # use cargo test in root dir will cause compile error, should be a cargo bug
 cargo test --features=mock 
 cd ..
