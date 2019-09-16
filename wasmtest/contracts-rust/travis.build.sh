6241c8e8becc0bace73ce24bb1bc8706b71151da
laizy <laizy@users.noreply.github.com>
2019-09-16 10:59:56 +0800

    implement wasm test case framework (#1066)
    * implement wasm test case framework
    
    * add runtime api check
    
    * fix testcase json decode
    
    * add jsvm run in wasm vm

---
+++ wasmtest/contracts-rust/travis.build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -x
+
+rm -rf js-vm && git clone --depth=1 https://github.com/laizy/ontio-js-vm js-vm
+cd js-vm
+RUSTFLAGS="-C link-arg=-zstack-size=32768" cargo build --lib --release --target=wasm32-unknown-unknown
+cd ..
+mv ./target/wasm32-unknown-unknown/release/boa.wasm ../testwasmdata/jsvm.wasm
+
+cd helloworld
+RUSTFLAGS="-C link-arg=-zstack-size=32768" cargo build --release --target=wasm32-unknown-unknown
+# use cargo test in root dir will cause compile error, should be a cargo bug
+cargo test --features=mock 
+cd ..
+
+cp ./target/wasm32-unknown-unknown/release/*.wasm ../testwasmdata
+
