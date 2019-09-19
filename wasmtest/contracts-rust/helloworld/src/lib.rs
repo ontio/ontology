5adf6dc77d62948e881396eaf05ae5bdbe286230
laizy <laizhichao@onchain.com>
2019-09-19 15:13:44 +0800

    pin ontio std version

--- wasmtest/contracts-rust/helloworld/src/lib.rs
+++ wasmtest/contracts-rust/helloworld/src/lib.rs
@@ -1,7 +1,7 @@
 #![cfg_attr(not(feature = "mock"), no_std)]
 #![feature(proc_macro_hygiene)]
 extern crate ontio_std as ostd;
-use ostd::abi::{Sink, ZeroCopySource};
+use ostd::abi::{Sink, Source};
 use ostd::prelude::*;
 use ostd::runtime;
 
@@ -13,7 +13,7 @@ pub fn add(a: U128, b: U128) -> U128 {
 #[no_mangle]
 pub fn invoke() {
     let input = runtime::input();
-    let mut source = ZeroCopySource::new(&input);
+    let mut source = Source::new(&input);
     let action: &[u8] = source.read().unwrap();
     let mut sink = Sink::new(12);
     match action {
