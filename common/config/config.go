4eb55a4c4acc3e3b352bc504ac613cf3d7e3c04a
steven <chenglin@ont.com>
2019-09-20 17:32:37 +0800

    update wasm gas factor

--- common/config/config.go
+++ common/config/config.go
@@ -70,7 +70,7 @@ const (
 	DEFUALT_CLI_RPC_ADDRESS                 = "127.0.0.1"
 	DEFAULT_GAS_LIMIT                       = 20000
 	DEFAULT_GAS_PRICE                       = 500
-	DEFAULT_WASM_GAS_FACTOR                 = uint64(5)
+	DEFAULT_WASM_GAS_FACTOR                 = uint64(10)
 
 	DEFAULT_DATA_DIR      = "./Chain"
 	DEFAULT_RESERVED_FILE = "./peers.rsv"
