e5ffd922f19c8252b871c2ca8b093f7ccdc00e15
laizy <laizy@users.noreply.github.com>
2019-09-26 14:25:11 +0800

    sort role func in auth contract (#1081)

--- smartcontract/service/native/auth/utils_test.go
+++ smartcontract/service/native/auth/utils_test.go
@@ -17,37 +17,14 @@
  */
 package auth
 
-import "testing"
+import (
+	"testing"
 
-//{"a", "b"} == {"b", "a"}
-func testEq(a, b []string) bool {
-	if a == nil && b == nil {
-		return true
-	}
-	if a == nil || b == nil {
-		return false
-	}
+	"github.com/stretchr/testify/assert"
+)
 
-	if len(a) != len(b) {
-		return false
-	}
-	Map := make(map[string]bool)
-	for i := range a {
-		Map[a[i]] = true
-	}
-	for _, s := range b {
-		_, ok := Map[s]
-		if !ok {
-			return false
-		}
-	}
-	return true
-}
 func TestStringSliceUniq(t *testing.T) {
-	s := []string{"foo", "foo1", "foo2", "foo", "foo1", "foo2", "foo3"}
-	ret := stringSliceUniq(s)
-	t.Log(ret)
-	if !testEq(ret, []string{"foo", "foo1", "foo2", "foo3"}) {
-		t.Fatalf("failed")
-	}
+	s := []string{"foo3", "foo", "foo1", "foo2", "foo", "foo1", "foo2", "foo3"}
+	ret := StringsDedupAndSort(s)
+	assert.Equal(t, ret, []string{"foo", "foo1", "foo2", "foo3"})
 }
