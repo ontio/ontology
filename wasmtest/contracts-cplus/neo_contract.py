16e99fbe00ba37375dea11400aa12cbea2d9b65d
steven <chenglin@ont.com>
2019-09-27 22:16:50 +0800

    add neovm call wasm, and neovm testframe

--- wasmtest/contracts-cplus/neo_contract.py
+++ wasmtest/contracts-cplus/neo_contract.py
@@ -1,5 +1,6 @@
 OntCversion = '2.0.0'
 from ontology.interop.System.ExecutionEngine import GetExecutingScriptHash
+from ontology.interop.Ontology.Wasm import InvokeWasm
 
 
 def Main(operation, args):
@@ -17,14 +18,68 @@ def Main(operation, args):
         return testh256(h256)
     elif operation == "neolist":
         return testlist(*args)
+    elif operation == "testcase":
+        return testcase()
+    elif operation == "add":
+        return add(args[0], args[1])
+    elif operation == "callwasm":
+        return testcallwasm(args[1])
     else:
         assert(False)
+
+
+def testcallwasm(textContext):
+    magicversion = b'\x00'
+    typebytearray = b'\x00'
+    typestring = b'\x01'
+    typeaddress = b'\x02'
+    typebool = b'\x03'
+    typeint = b'\x04'
+    typeh256 = b'\x05'
+    typelist = b'\x10'
+
+    # uint32 lsize = 2
+    lsize = b'\x03\x00\x00\x00'
+    magicversion = concat(magicversion, typelist)
+    magicversion = concat(magicversion, lsize)
+
+    #int128 a = 2, int 128 b = 3. base len is 15bytes
+    base = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    magicversion = concat(magicversion, typestring)
+    magicversion = concat(magicversion, '\x03\x00\x00\x00')
+    magicversion = concat(magicversion, "add")
+
+    a = concat('\x02', base)
+    b = concat('\x03', base)
+    magicversion = concat(magicversion, typeint)
+    magicversion = concat(magicversion, a)
+    magicversion = concat(magicversion, typeint)
+    magicversion = concat(magicversion, b)
+    
+    address = textContext[0]["test_add.wasm"]
+    res = InvokeWasm(address, magicversion)
+    c = concat('\x05', base)
+    assert(res == c)
+    return c+0
+
+
+def testcase():
+    return '''
+    [
+        [{"needcontext":false,"env":{"witness":[]}, "method":"add", "param":"[int:1,int:2]", "expected":"int:3"},
+        {"needcontext":true,"env":{"witness":[]}, "method":"callwasm", "param":"[int:1]", "expected":"int:5"}
+        ]
+    ]'''
 
 
 def testbytearray(addr):
     assert(GetExecutingScriptHash() == addr)
     assert(len(addr) == 20)
     return addr
+
+
+def add(a, b):
+    return a + b
 
 
 def testboolean(istrue):
