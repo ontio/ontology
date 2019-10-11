24bc14d4e79ffde67838015048b4f1ffecb469f8
steven <chenglin@ont.com>
2019-10-11 19:26:19 +0800

    remove putContract return

--- smartcontract/storage/cachedb.go
+++ smartcontract/storage/cachedb.go
@@ -100,7 +100,7 @@ func (self *CacheDB) GetContract(addr comm.Address) (*payload.DeployCode, error)
 	return contract, nil
 }
 
-func (self *CacheDB) PutContract(contract *payload.DeployCode) error {
+func (self *CacheDB) PutContract(contract *payload.DeployCode) {
 	address := contract.Address()
 
 	sink := comm.NewZeroCopySink(nil)
@@ -108,7 +108,6 @@ func (self *CacheDB) PutContract(contract *payload.DeployCode) error {
 
 	value := sink.Bytes()
 	self.put(common.ST_CONTRACT, address[:], value)
-	return nil
 }
 
 func (self *CacheDB) DeleteContract(address comm.Address) {
