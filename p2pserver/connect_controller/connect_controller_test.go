9953ddcf3e75f216f2470e35b1d4b39e1f456e9b
laizy <aochyi@126.com>
2020-06-29 14:04:23 +0800

    format

--- p2pserver/connect_controller/connect_controller_test.go
+++ p2pserver/connect_controller/connect_controller_test.go
@@ -24,7 +24,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/ontio/ontology/common/log"
 	"github.com/ontio/ontology/p2pserver/common"
 	"github.com/ontio/ontology/p2pserver/handshake"
 	"github.com/ontio/ontology/p2pserver/peer"
