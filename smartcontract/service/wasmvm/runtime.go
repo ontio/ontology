29a52ea074a7fc995b1824d98c7586abb1941303
Allen <lvshuaino@gmail.com>
2020-11-24 15:35:12 +0800

    add wasm debug info

--- smartcontract/service/wasmvm/runtime.go
+++ smartcontract/service/wasmvm/runtime.go
@@ -163,8 +163,7 @@ func Debug(proc *exec.Process, ptr uint32, len uint32) {
 		//do not panic on debug
 		return
 	}
-	log.Debugf("[WasmContract]Step:%s\n", proc.HostData().(*Runtime).Service.ExecStep)
-	debugLog(bs)
+	log.Debugf("[WasmContract]Debug:%s Step %v\n", bs, proc.HostData().(*Runtime).Service.ExecStep)
 }
 
 func notify(service *WasmVmService, bs []byte) error {
