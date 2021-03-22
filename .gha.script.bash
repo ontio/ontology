55a382e7ad0ce8ab0c5b27a8a3150a6b5759f945
rongyi <rongyi@onchain.com>
2021-03-22 14:39:01 +0800

    Initial commit for githubaction (#1300)
    disable wasm test

---
+++ .gha.script.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -ex
+
+VERSION=$(git describe --always --tags --long)
+
+if [ $RUNNER_OS == 'Linux' ]; then
+  echo "linux sys"
+  env GO111MODULE=on make all
+  env GO111MODULE=on go mod vendor
+  #cd ./wasmtest && bash ./gha-run-wasm-tests.sh && cd ../
+  bash ./.gha.check-license.sh
+  bash ./.gha.check-templog.sh
+  bash ./.gha.gofmt.sh
+  bash ./.gha.gotest.sh
+elif [ $RUNNER_OS == 'osx' ]; then
+  echo "osx sys"
+  env GO111MODULE=on make all
+else
+  echo "win sys"
+  env GO111MODULE=on CGO_ENABLED=1 go build  -ldflags "-X github.com/ontio/ontology/common/config.Version=${VERSION}" -o ontology-windows-amd64 main.go
+  env GO111MODULE=on go build  -ldflags "-X github.com/ontio/ontology/common/config.Version=${VERSION}" -o sigsvr-windows-amd64 sigsvr.go
+fi
