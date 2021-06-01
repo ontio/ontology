ca71527d9ad9239059c5d44e5243d2697a47dfe9
laizy <aochyi@126.com>
2021-06-01 19:07:54 +0800

    init evm service

--- smartcontract/smart_contract.go
+++ smartcontract/smart_contract.go
@@ -20,6 +20,7 @@ package smartcontract
 import (
 	"errors"
 	"fmt"
+	"github.com/ontio/ontology/smartcontract/service/evm"
 
 	"github.com/ontio/ontology/common"
 	"github.com/ontio/ontology/common/config"
@@ -190,6 +191,18 @@ func (this *SmartContract) NewExecuteEngine(code []byte, txtype ctypes.Transacti
 			GasFactor:  gasFactor,
 			JitMode:    this.JitMode,
 		}
+	case ctypes.EIP155:
+		service = &evm.EvmService{
+			Store:      this.Store,
+			CacheDB:    this.CacheDB,
+			Code:       code,
+			Tx:         this.Config.Tx,
+			ContextRef: this,
+			Time:       this.Config.Time,
+			Height:     this.Config.Height,
+			BlockHash:  this.Config.BlockHash,
+		}
+
 	default:
 		return nil, errors.New("failed to construct execute engine, wrong transaction type")
 	}
