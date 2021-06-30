7b62d059266e87bb0459f79818c770c0d15831c9
lucas7788 <sishsh@163.com>
2021-06-30 21:23:17 +0800

    add license

--- integrationtest/common.go
+++ integrationtest/common.go
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2018 The ontology Authors
+ * This file is part of The ontology library.
+ *
+ * The ontology is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ontology is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package integrationtest
 
 import (
@@ -94,7 +111,7 @@ func makeBlock(acc *account.Account, txs []*types.Transaction) (*types.Block, er
 	return block, nil
 }
 
-func newNativeTx(contractAddress common.Address, version byte,gasPrice,gasLimit uint64, method string, params []interface{}) *types.MutableTransaction {
+func newNativeTx(contractAddress common.Address, version byte, gasPrice, gasLimit uint64, method string, params []interface{}) *types.MutableTransaction {
 	invokeCode, err := cutils.BuildNativeInvokeCode(contractAddress, version, method, params)
 	checkErr(err)
 	invokePayload := &payload.InvokeCode{
@@ -110,7 +127,6 @@ func newNativeTx(contractAddress common.Address, version byte,gasPrice,gasLimit 
 	}
 	return tx
 }
-
 
 func checkErr(err error) {
 	if err != nil {
