1010dd00460742288f430f40129858eb58901f86
rongyi <rongyi@onchain.com>
2022-02-10 11:25:28 +0800

    Update test script

--- .gha.gotest.sh
+++ .gha.gotest.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# code from https://github.com/Seklfreak/Robyul2
-unset dirs files
-dirs=$(go list ./... | grep -v vendor/ | grep -v ontology$)
-set -x -e
 
-for d in $dirs
-do
-  go test -v $d
-done
+go test -v ./...
