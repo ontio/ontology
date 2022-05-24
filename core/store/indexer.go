965eaa920300d9ba3b57b490e87710170a80d9b0
Allen <lvshuaino@gmail.com>
2022-05-24 10:11:40 +0800

    fix

--- core/store/indexer.go
+++ core/store/indexer.go
@@ -80,10 +80,7 @@ func (i *Indexer) ProcessSection(k LedgerStore, blockHeight uint32) error {
 
 		var err error
 		// Reset and partial processing
-		if err = i.backend.Reset(section); err != nil {
-			err = i.setValidSections(0)
-			return fmt.Errorf(err.Error())
-		}
+		i.backend.Reset(section)
 
 		begin := section*BloomBitsBlocks + start
 		end := (section+1)*BloomBitsBlocks + start
