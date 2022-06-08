c25863a2f295a1e846f4418429cf3aa457194504
Allen <lvshuaino@gmail.com>
2022-06-08 10:11:00 +0800

    fix

--- core/store/ledgerstore/bloombits.go
+++ core/store/ledgerstore/bloombits.go
@@ -76,6 +76,16 @@ func NewBloomIndexer(store *leveldbstore.LevelDBStore, section uint32) bloomInde
 	return b
 }
 
+// Reset implements core.ChainIndexerBackend, starting a new bloombits index
+// section.
+//func (b *bloomIndexer) Reset(section uint32) {
+//	gen, err := bloombits.NewGenerator(uint(BloomBitsBlocks))
+//	if err != nil {
+//		panic(err) // never fired since BloomBitsBlocks is multiple of 8
+//	}
+//	b.gen, b.section = gen, section
+//}
+
 // Process implements core.ChainIndexerBackend, adding a new header's bloom into
 // the index.
 func (b *bloomIndexer) Process(height uint32, bloom types.Bloom) {
@@ -93,6 +103,7 @@ func (b *bloomIndexer) BatchPut() {
 		value := bitutil.CompressBytes(bits)
 		b.store.BatchPut(bloomBitsKey(uint(i), b.section), value)
 	}
+	b.section++
 }
 
 func (this *bloomIndexer) PutFilterStart(height uint32) error {
