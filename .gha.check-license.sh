a0ebc0c8858a6a736316b724d0f670a1bef1ced8
robinsdan <robinsdan1111@hotmail.com>
2025-11-26 10:58:23 +0800

    skip license check for eth tracers file

--- .gha.check-license.sh
+++ .gha.check-license.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 unset dirs files
+FILTER_PATHS=(
+    "/ethrpc/tracers"  
+    "/ethrpc/debug"  
+)
 dirs=$(go list -f {{.Dir}} ./... | grep -v /vendor/)
 for d in $dirs
 do
+    skip=0
+    for filter in "${FILTER_PATHS[@]}"; do
+        if [[ "$d" == *"$filter"* ]]; then
+            skip=1
+            break  
+        fi
+    done
+    [[ $skip -eq 1 ]] && continue
+
     for f in $d/*.go
     do
     grep -q "Copyright (C) 20[1-2][0-9] The [o|O]ntology Authors" $f || files="${files} $f"
